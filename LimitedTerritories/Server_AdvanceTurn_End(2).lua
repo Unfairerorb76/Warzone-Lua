ec24aa2b9b268b19a21b4d0026bdbefde6e99a76
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-02-11 16:23:13 +0000

    Create Server_AdvanceTurn_End(2).lua

---
+++ LimitedTerritories/Server_AdvanceTurn_End(2).lua
@@ -0,0 +1,41 @@
+
+
+
+function Server_AdvanceTurn_End(game, addNewOrder)
+  
+for _, limitTerritories(game.ServerGame.LatestTurnStanding.Territories) do 
+ 
+    local count = 0;
+    
+    players = {};
+    
+     for _, playerID in pairs(game.ServerGame.Game.PlayingPlayers) do
+       players[PlayerID] = {};
+     end
+  
+     for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+            if (terr.IsNeutral == false) then
+                   players[terr.OwnerPlayerID][terr.ID] = terr.NumArmies.NumArmies;
+                   count = count + 1; 
+               end               
+     end
+    
+    
+--after this point, uncertainty and copy & pasting lies --
+    
+    
+ local armiesOnTerritory = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID].NumArmies.NumArmies;
+ 
+      
+      if (count > Mod.Settings.Limit) then
+         if (armiesOnTerritory < players[terr.OwnerPlayerID][terr.ID])
+            then armiesOnTerritory = players[terr.OwnerPlayerID]
+        
+      local terr = game.ServerGame.LatestTurnStanding.Territories[order.To];
+			terrMod = WL.TerritoryModification.Create(terr.ID);
+			terrMod.SetOwnerOpt=terr.OwnerPlayerID;
+			terrMod.SetArmiesTo =  game.ServerGame.LatestTurnStanding.Territories[order.To];
+          
+      
+      
+ end
