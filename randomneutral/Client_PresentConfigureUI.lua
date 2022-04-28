8c00a3ad6a3bad62a0797afcb9b5f7d83f5f3e86
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-04-28 12:18:56 +0100

    Update Client_PresentConfigureUI.lua

--- randomneutral/Client_PresentConfigureUI.lua
+++ randomneutral/Client_PresentConfigureUI.lua
@@ -1,15 +1,15 @@
 
 function Client_PresentConfigureUI(rootParent)
-	local initialValue = Mod.Settings.CostPerNeutralArmy;
+	local initialValue = Mod.Settings.NumToConvert;
 	if initialValue == nil then
-		initialValue = 3;
+		initialValue = 2;
 	end
     
     local horz = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz).SetText('Cost per neutral army on the territory');
+	UI.CreateLabel(horz).SetText('amount of neutrals a player shall gain each turn');
     numberInputField = UI.CreateNumberInputField(horz)
 		.SetSliderMinValue(1)
-		.SetSliderMaxValue(8)
+		.SetSliderMaxValue(10)
 		.SetValue(initialValue);
 
 end
