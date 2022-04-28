6ec8495b2dae4629c0ae7edea550e8b662299f99
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-04-28 14:58:04 +0100

    Update Server_AdvanceTurn.lua

--- randomneutral/Server_AdvanceTurn.lua
+++ randomneutral/Server_AdvanceTurn.lua
@@ -3,29 +3,21 @@ require('WLUtilities');
 
 function Server_EndTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	
-      local neutralTerr = {};
+      local terr = {};
 for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
   if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
-    neutralTerr[#neutralTerr+1] = terrID;
+    terr[#terr+1] = terrID;
   end
 end
 for i=1,Mod.Settings.NumToConvert do
+     randomNeutralTerr = neutralTerr[Math.random(#neutralTerr)];
+	terrMod = WL.TerritoryModification.Create(randomNeutralTerr);
+	terrMod.SetOwnerOpt=terr.OwnerPlayerID;
+	terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies.NumArmies;
+	addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"new territory",{},{terrMod}));
 		
-		local terr = game.ServerGame.LatestTurnStanding.Territories[order.To];
-			terrMod = WL.TerritoryModification.Create(terr.ID);
-			terrMod.SetOwnerOpt=terr.OwnerPlayerID;
-			terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies.NumArmies;
-			addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"new territory",{},{terrMod}));
-		
-  randomNeutralTerr = neutralTerr[Math.random(#neutralTerr)];
+  
   game.ServerGame.LatestTurnStanding.Territories[randomNeutralTerr].OwnerPlayerID = PlayerID;
 end
 	
 end
-
-
-local terr = game.ServerGame.LatestTurnStanding.Territories[order.To];
-			terrMod = WL.TerritoryModification.Create(terr.ID);
-			terrMod.SetOwnerOpt=terr.OwnerPlayerID;
-			terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies.NumArmies;
-			addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"new territory",{},{terrMod}));
