cd4684be58c34f26c172d4cb93c82b42b7974f0d
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-04-28 14:58:27 +0100

    Update Server_AdvanceTurn.lua

--- randomneutral/Server_AdvanceTurn.lua
+++ randomneutral/Server_AdvanceTurn.lua
@@ -10,13 +10,13 @@ for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) d
   end
 end
 for i=1,Mod.Settings.NumToConvert do
+		
      randomNeutralTerr = neutralTerr[Math.random(#neutralTerr)];
 	terrMod = WL.TerritoryModification.Create(randomNeutralTerr);
 	terrMod.SetOwnerOpt=terr.OwnerPlayerID;
 	terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies.NumArmies;
 	addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"new territory",{},{terrMod}));
 		
-  
   game.ServerGame.LatestTurnStanding.Territories[randomNeutralTerr].OwnerPlayerID = PlayerID;
 end
 	
