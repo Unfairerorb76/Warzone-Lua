e03fd7304097cf9cb081dbac133f8083ed175ed3
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-04-28 12:17:45 +0100

    Update Client_PresentSettingsUI.lua

--- randomneutral/Client_PresentSettingsUI.lua
+++ randomneutral/Client_PresentSettingsUI.lua
@@ -1,6 +1,6 @@
 
 function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(rootParent)
-		.SetText('Cost per neutral army = ' .. Mod.Settings.CostPerNeutralArmy);
+		.SetText('amount of neutrals you get for free every turn = ' .. Mod.Settings.NumToConvert);
 end
 
