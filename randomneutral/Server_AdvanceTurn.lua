29719808a6870080f394cc6531f6dc4a676b0643
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-04-28 15:14:50 +0100

    Update Server_AdvanceTurn.lua

--- randomneutral/Server_AdvanceTurn.lua
+++ randomneutral/Server_AdvanceTurn.lua
@@ -3,13 +3,15 @@ require('WLUtilities');
 
 function Server_EndTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	
-      local terr = {};
-for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+local terr = {};
+	
+for i=1,Mod.Settings.NumToConvert do
+		
+	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
   if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
     terr[#terr+1] = terrID;
   end
-end
-for i=1,Mod.Settings.NumToConvert do
+end			
 		
      randomNeutralTerr = terr[Math.random(#terr)];
 	terrMod = WL.TerritoryModification.Create(randomNeutralTerr);
