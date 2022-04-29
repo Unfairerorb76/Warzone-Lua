5b9b1d21d2c936cc1659f7a68bb242ce206536bb
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-04-29 12:53:52 +0100

    Update Client_PresentConfigureUI.lua

--- randomneutral/Client_PresentConfigureUI.lua
+++ randomneutral/Client_PresentConfigureUI.lua
@@ -23,8 +23,8 @@ local horz2 = UI.CreateHorizontalLayoutGroup(vert);
 		.SetValue(initialValueConvert);
 	
 	    
-	UI.CreateLabel(horz2).SetText('amount of armies a player shall get with the territory');
-    numberInputField = UI.CreateNumberInputField(horz2);
+	UI.CreateLabel(vert).SetText('amount of armies a player shall get with the territory');
+    numberInputField = UI.CreateNumberInputField(vert);
 	        
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(10)
