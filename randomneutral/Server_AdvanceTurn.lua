f80cdbf2dbfe4a8c97e85b2a7bf366e02e980fb2
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-05-02 15:51:33 +0200

    next try, did some code reduction

--- randomneutral/Server_AdvanceTurn.lua
+++ randomneutral/Server_AdvanceTurn.lua
@@ -8,53 +8,34 @@ function Server_AdvanceTurn_End(game, addNewOrder)
         local nonDistArmies = Game.Settings.InitialNonDistributionArmies;
 	If (Mod.Settings.OnlyBaseNeutrals == false) do
 		for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-    		if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
-			
-      			table.insert(terr, terrID);   --gets each territory ID of neutrals
+    			if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
+      				table.insert(terr, terrID);   --gets each territory ID of neutrals
 			end
 		end			
-
-			
-		for times = 1, math.min(Mod.Settings.NumToConvert, math.floor(#terr / getTableLength(game.ServerGame.Game.PlayingPlayers))) do
-			print(times);
-  			for i, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
-				print(4);
-				local rand = math.random(#terr);
-				local randomNeutralTerr = terr[rand]; --picks random neutral then gives it too player
-				if randomNeutralTerr == nill then break; end
-				local terrMod = WL.TerritoryModification.Create(randomNeutralTerr);   
-
-				terrMod.SetOwnerOpt = i;
-         		terrMod.SetArmiesTo = Mod.Settings.SetArmiesTo; -- you can leave this out, if this field is nill it will not change anything to the army count
-				addNewOrder(WL.GameOrderEvent.Create(i,"new territory",{},{terrMod}), true);
-				table.remove(terr, rand);
-			end	
-		end	
-If (Mod.Settings.OnlyBaseNeutrals == true) do	
+	else
 		for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-    		if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
-		     if (NumArmies == nonDistArmies) then
-
-      			table.insert(terr, terrID);   --gets each territory ID of neutrals
+    			if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
+		    		if (NumArmies == nonDistArmies) then
+      					table.insert(terr, terrID);   --gets each territory ID of neutrals
+				end
 			end
 		end			
-			
-		for times = 1, math.min(Mod.Settings.NumToConvert, math.floor(#terr / getTableLength(game.ServerGame.Game.PlayingPlayers))) do
-			print(times);
-  			for i, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
-				print(4);
-				local rand = math.random(#terr);
-				local randomNeutralTerr = terr[rand]; --picks random neutral then gives it too player
-				if randomNeutralTerr == nill then break; end
-				local terrMod = WL.TerritoryModification.Create(randomNeutralTerr);   
-
-				terrMod.SetOwnerOpt = i;
-         		terrMod.SetArmiesTo = Mod.Settings.SetArmiesTo; -- you can leave this out, if this field is nill it will not change anything to the army count
-				addNewOrder(WL.GameOrderEvent.Create(i,"new territory",{},{terrMod}), true);
-				table.remove(terr, rand);
-			end	
-		end	
 	end
+	for times = 1, math.min(Mod.Settings.NumToConvert, math.floor(#terr / getTableLength(game.ServerGame.Game.PlayingPlayers))) do
+		print(times);
+		for i, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
+			print(4);
+			local rand = math.random(#terr);
+			local randomNeutralTerr = terr[rand]; --picks random neutral then gives it too player
+			if randomNeutralTerr == nill then break; end
+			local terrMod = WL.TerritoryModification.Create(randomNeutralTerr);   
+
+			terrMod.SetOwnerOpt = i;
+			terrMod.SetArmiesTo = Mod.Settings.SetArmiesTo; -- you can leave this out, if this field is nill it will not change anything to the army count
+			addNewOrder(WL.GameOrderEvent.Create(i,"new territory",{},{terrMod}), true);
+			table.remove(terr, rand);
+		end	
+	end	
 end
 
 
