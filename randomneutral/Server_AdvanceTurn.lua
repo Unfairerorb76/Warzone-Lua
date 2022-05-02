e1c66e9ebd03fe931651991a2de497ad27f5ffc8
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-02 20:23:07 +0100

    Update Server_AdvanceTurn.lua

--- randomneutral/Server_AdvanceTurn.lua
+++ randomneutral/Server_AdvanceTurn.lua
@@ -6,7 +6,7 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 	local terr = {};  --table of neutral territories
 	local randomNeutralTerr;   
         local nonDistArmies = game.Settings.InitialNonDistributionArmies;
-	If (Mod.Settings.OnlyBaseNeutrals == false) do
+	If (Mod.Settings.OnlyBaseNeutrals == false) then
 		for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
     		if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
 			
