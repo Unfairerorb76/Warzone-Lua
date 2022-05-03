6ea90a546c1853ccad72bd5815b2dc4e33288aab
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-03 15:09:47 +0100

    Update Server_AdvanceTurn.lua

--- randomneutral/Server_AdvanceTurn.lua
+++ randomneutral/Server_AdvanceTurn.lua
@@ -7,8 +7,8 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 	local randomNeutralTerr;   
         local nonDistArmies = game.Settings.InitialNonDistributionArmies;
   
-if (Mod.Settings.OnlyBaseNeutrals = nil) then
-      Mod.Settings.OnlyBaseNeutrals = false; end
+if (Mod.Settings.OnlyBaseNeutrals == nil) then
+      Mod.Settings.OnlyBaseNeutrals == false; end
 
 
     print(Mod.Settings.OnlyBaseNeutrals);
