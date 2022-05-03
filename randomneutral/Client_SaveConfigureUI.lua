d78413fdbb7138a0f3d8e5d3d6d9dc8cf45e1196
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-03 14:49:10 +0100

    Update Client_SaveConfigureUI.lua

--- randomneutral/Client_SaveConfigureUI.lua
+++ randomneutral/Client_SaveConfigureUI.lua
@@ -4,4 +4,5 @@ function Client_SaveConfigureUI(alert)
     Mod.Settings.NumToConvert = numberInputField.GetValue();
     Mod.Settings.SetArmiesTo = numberInputField.GetValue();
     Mod.Settings.OnlyBaseNeutrals = booleanInputField.GetIsChecked();
+  print(Mod.Settings.OnlyBaseNeutrals);
 end
