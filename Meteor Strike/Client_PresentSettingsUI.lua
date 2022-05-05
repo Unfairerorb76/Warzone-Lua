11aa0730f2992cffcf06c786257e3729c8657efe
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-05 13:01:29 +0100

    Update Client_PresentSettingsUI.lua

--- Meteor Strike/Client_PresentSettingsUI.lua
+++ Meteor Strike/Client_PresentSettingsUI.lua
@@ -1,11 +1,10 @@
 
 function Client_PresentSettingsUI(rootParent)
-	print(2);
+	
 
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
-UI.CreateLabel(vert).SetText('amount of territories(neutrals) you get for free every turn = ' .. Mod.Settings.NumToConvert);
-UI.CreateLabel(vert).SetText('amount of armies that come with each new territory = ' .. Mod.Settings.SetArmiesTo);
-UI.CreateLabel(vert).SetText('Only claim base neutrals = ' .. Mod.Settings.OnlyBaseNeutrals);
+UI.CreateLabel(vert).SetText('amount of territories that will be hit per turn = ' .. Mod.Settings.NumOfStrikes);
+UI.CreateLabel(vert).SetText('amount of armies that will die per hit = ' .. Mod.Settings.ArmiesKilled);
 
 end
 
