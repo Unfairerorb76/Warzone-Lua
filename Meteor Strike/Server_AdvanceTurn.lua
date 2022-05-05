4d5e74043d8c1e69185bf6cf78392e222b858b6c
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-05 14:34:47 +0100

    Update Server_AdvanceTurn.lua

--- Meteor Strike/Server_AdvanceTurn.lua
+++ Meteor Strike/Server_AdvanceTurn.lua
@@ -23,7 +23,7 @@ for times = 1, Mod.Settings.NumOfStrikes do
 			local randomNeutralTerr = terr[rand]; --picks random neutral then gives it too player
 			local terrMod = WL.TerritoryModification.Create(randomNeutralTerr);   
                      print(Mod.Settings.ArmiesKilled);
-         		terrMod.SetArmiesTo = Mod.Settings.ArmiesKilled; 
+         		terrMod.SetArmiesTo = max(0,(territory.NumArmies.NumArmies - Mod.Settings.ArmiesKilled)); 
 				addNewOrder(WL.GameOrderEvent.Create(i,"meteor strike",{},{terrMod}), true);
 				table.remove(terr, rand);
 	            print(addNewOrder);
