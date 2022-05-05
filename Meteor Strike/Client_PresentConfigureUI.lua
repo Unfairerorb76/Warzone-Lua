583a830fdc354d1a0112bbf38e8b956cdf932655
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-05 12:59:55 +0100

    Update Client_PresentConfigureUI.lua

--- Meteor Strike/Client_PresentConfigureUI.lua
+++ Meteor Strike/Client_PresentConfigureUI.lua
@@ -1,41 +1,35 @@
 
 function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 	print(1);
-	local initialValueConvert = Mod.Settings.NumToConvert;
-	local initialValueArmies = Mod.Settings.SetArmiesTo;
-        local initalcheckbox = Mod.Settings.OnlyBaseNeutrals;
+	local initialValueStrikes = Mod.Settings.NumOfStrikes;
+	local initialValueKilled = Mod.Settings.SetArmiesTo;
+        
 	
-	if initialValueConvert == nil then
-		initialValueConvert = 2;
+	if initialValueStrikes == nil then
+		initialValueStrikes = 5;
 	end
 	
-	if initialValueArmies == nil then
-		initialValueArmies = 2;
+	if initialValueKilled == nil then
+		initialValueKilled = 2;
 	end
-
-        if initalcheckbox == nil then 
-                initalcheckbox = false; 
-        end
     
  local vert = UI.CreateVerticalLayoutGroup(rootParent);
 local horz1 = UI.CreateHorizontalLayoutGroup(vert);  --not used
 local horz2 = UI.CreateHorizontalLayoutGroup(vert);  --not used but here for reference
 	
-	UI.CreateLabel(vert).SetText('amount of neutrals a player shall gain each turn');
+	UI.CreateLabel(vert).SetText('Amount of meteors that will hit each turn');
     numberInputField = UI.CreateNumberInputField(vert)
 		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(initialValueConvert);
+		.SetSliderMaxValue(20)
+		.SetValue(initialValueStrikes);
 	
-	 UI.CreateLabel(vert).SetText('amount of armies a player shall get with the territory');
+	 UI.CreateLabel(vert).SetText('amount of armies that will die per strike');
     numberInputField2 = UI.CreateNumberInputField(vert)
 		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(initialValueArmies); 
+		.SetSliderMaxValue(15)
+		.SetValue(initialValueKilled); 
  
-        UI.CreateLabel(vert).SetText('only base neutrals shall be claimed');
-        booleanInputField = UI.CreateCheckBox(vert)        
-                .SetIsChecked(initalcheckbox);
+ 
                 
 
 end
