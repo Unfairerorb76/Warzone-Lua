767ce59f414f544ce1f4b20643959b234941dac8
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-05 12:33:59 +0100

    Add files via upload

---
+++ Meteor Strike/Client_SaveConfigureUI.lua
@@ -0,0 +1,8 @@
+
+function Client_SaveConfigureUI(alert)
+    
+    Mod.Settings.NumToConvert = numberInputField.GetValue();
+    Mod.Settings.SetArmiesTo = numberInputField2.GetValue();
+    Mod.Settings.OnlyBaseNeutrals = booleanInputField.GetIsChecked();
+  print(Mod.Settings.OnlyBaseNeutrals);
+end
