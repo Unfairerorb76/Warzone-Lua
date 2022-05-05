cd72dcd8e91607000c052aa65302f7692d472e65
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-05 11:25:08 +0100

    Update Client_PresentSettingsUI.lua

--- randomneutral/Client_PresentSettingsUI.lua
+++ randomneutral/Client_PresentSettingsUI.lua
@@ -4,7 +4,7 @@ function Client_PresentSettingsUI(rootParent)
 
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
 UI.CreateLabel(vert).SetText('amount of territories(neutrals) you get for free every turn = ' .. Mod.Settings.NumToConvert);
-UI.CreateLabel(vert).SetText('amount of armies that come with each new territory = ' .. Mod.Settings.SetArmiesTo
+UI.CreateLabel(vert).SetText('amount of armies that come with each new territory = ' .. Mod.Settings.SetArmiesTo);
 UI.CreateLabel(vert).SetText('Only claim base neutrals = ' .. Mod.Settings.OnlyBaseNeutrals);
 
 end
