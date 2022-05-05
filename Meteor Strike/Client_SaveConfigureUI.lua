6d03afb93f749b721da80c4e006792d8732a9c36
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-05 13:21:23 +0100

    Update Client_SaveConfigureUI.lua

--- Meteor Strike/Client_SaveConfigureUI.lua
+++ Meteor Strike/Client_SaveConfigureUI.lua
@@ -3,5 +3,6 @@ function Client_SaveConfigureUI(alert)
     
     Mod.Settings.NumOfStrikes = numberInputField.GetValue();
     Mod.Settings.ArmiesKilled = numberInputField2.GetValue();
-
+   print(ArmiesKilled);
+   print(NumOfStrikes);
 end
