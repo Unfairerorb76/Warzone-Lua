9ec6d78ca251f96ebbe7e7fa1c39b1d172f2b0f1
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-09 15:12:15 +0100

    Update Client_SaveConfigureUI.lua

--- Meteor Strike/Client_SaveConfigureUI.lua
+++ Meteor Strike/Client_SaveConfigureUI.lua
@@ -4,8 +4,8 @@ function Client_SaveConfigureUI(alert)
     Mod.Settings.NumOfStrikes = numberInputField.GetValue();
     Mod.Settings.ArmiesKilled = numberInputField2.GetValue();
     Mod.Settings.EnableDoomsDay = booleanInputField.GetIsChecked();
-    Mod.Settings.TerrSurvived = numberInputField3.GetValue();
-    Mod.Settings.TurnDoomsDay = numberInputField4.GetValue();
+    Mod.Settings.TerrSurvived = numberInputField4.GetValue();
+    Mod.Settings.TurnDoomsDay = numberInputField3.GetValue();
     
     
 end
