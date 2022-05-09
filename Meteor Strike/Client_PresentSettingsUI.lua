d0afe560dd95e540ff4ec93b59d902e5cc5c18c9
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-05-09 13:45:18 +0100

    Update Client_PresentSettingsUI.lua

--- Meteor Strike/Client_PresentSettingsUI.lua
+++ Meteor Strike/Client_PresentSettingsUI.lua
@@ -6,5 +6,9 @@ local vert = UI.CreateVerticalLayoutGroup(rootParent);
 UI.CreateLabel(vert).SetText('amount of territories that will be hit per turn = ' .. Mod.Settings.NumOfStrikes);
 UI.CreateLabel(vert).SetText('amount of armies that will die per hit = ' .. Mod.Settings.ArmiesKilled);
 
+if (Mod.Settings.EnableDoomsDay == true) then
+UI.CreateLabel(vert).SetText('the turn of which the final meteor hits = ' .. Mod.Settings.TurnDoomsDay);
+UI.CreateLabel(vert).SetText('amount of territories that will survive the final hit = ' .. Mod.Settings.TerrSurvived);
+end
 end
 
