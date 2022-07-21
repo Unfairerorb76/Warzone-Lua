ad328afbb08691b777f60133ee1ee0cd1dfd17c6
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-21 16:47:34 +0100

    Update Client_PresentConfigureUI.lua

--- Villages/Client_PresentConfigureUI.lua
+++ Villages/Client_PresentConfigureUI.lua
@@ -1,5 +1,5 @@
 function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
-	
+	print(1);
 	local initialVillages = Mod.Settings.NumOfVillages;
 	
 	if initialVillages == nil then
@@ -16,5 +16,5 @@ function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(10)
 		.SetValue(initialVillages);
-        
+       print(2); 
 end
