b11ba6ca476cc1945196dadfebbb46b26bcbeb69
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-21 16:55:20 +0100

    Update Server_AdvanceTurn.lua

--- Villages/Server_AdvanceTurn.lua
+++ Villages/Server_AdvanceTurn.lua
@@ -1,45 +1,6 @@
 require('Utilities');
 require('WLUtilities');
 
-function Server_AdvanceTurn_Start(game, addNewOrder)
-
-  local terr = {}; --table of neutral territories
-  local randomNeutralTerr; 
-	
-	
-if (game.ServerGame.Game.TurnNumber == 1) then	
-   
-	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do	
-		table.insert(terr, terrID);   --gets each territory ID of neutrals
-	end
-	
-amountOfVillages = Mod.Settings.NumOfVillages
-
-	for i = 1, amountOfVillages do
-		local rand = math.random(#terr);
-		local structures = standing.Territories[Terr[rand]].Structures;
-		if structures == nil then 
-			structures = {}; 
-			structures[structure] = 1;
-		else
-			if structures[structure] == nil then
-				structures[structure] = 1;
-			else
-				structures[structure] = structures[structure] + 1;
-			end
-		end
-		standing.Territories[listOfTerr[rand]].Structures = structures;
-		table.remove(terr, rand)
-	end
-	return structure;
-	
-	
-	
-	
-	
-	end -- end of turn number if statement
-end -- end of function
-
 function Server_AdvanceTurn_End(game, addNewOrder)
 
   
