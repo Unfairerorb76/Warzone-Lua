52eabf70535941f34f0330202d1cd9b034e19fed
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-21 15:27:16 +0100

    Update Client_PresentSettingsUI.lua

--- Villages/Client_PresentSettingsUI.lua
+++ Villages/Client_PresentSettingsUI.lua
@@ -1,1 +1,7 @@
+function Client_PresentSettingsUI(rootParent)
+	
 
+local vert = UI.CreateVerticalLayoutGroup(rootParent);	
+UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages);
+
+end
