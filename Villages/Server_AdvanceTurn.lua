7a359612c5dd0bc0f398921c7cbb110f670bff0d
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-21 15:31:41 +0100

    Update Server_AdvanceTurn.lua

--- Villages/Server_AdvanceTurn.lua
+++ Villages/Server_AdvanceTurn.lua
@@ -1,9 +1,29 @@
-
+require('Utilities');
+require('WLUtilities');
 
 function Server_AdvanceTurn_Start(game, addNewOrder)
 
+  
+  
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
 
+  
+  
 end
+
+
+
+
+
+
+function getTableLength(t)
+	local a = 0;
+	for i, _ in pairs(t) do
+		
+		a = a + 1;
+	end
+	return a;
+end
+
