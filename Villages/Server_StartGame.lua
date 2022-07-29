03e46897b62f301a1b5839ce8fb45f7c62384cb7
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-29 14:05:07 +0100

    Update Server_StartGame.lua

--- Villages/Server_StartGame.lua
+++ Villages/Server_StartGame.lua
@@ -3,9 +3,9 @@ print(1);
   local terr = {}; --table of neutral territories
   local randomNeutralTerr; 	
    
-	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do	
+	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
 		table.insert(terr, terrID);   --gets each territory ID of neutrals
-	end
+			end
 	
 print(2);	
 amountOfVillages = Mod.Settings.NumOfVillages
