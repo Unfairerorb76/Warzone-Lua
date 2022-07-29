8401c57028bed3dda43728604d5a4a8aa32ca247
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-29 14:29:06 +0100

    Update Server_StartGame.lua

--- Villages/Server_StartGame.lua
+++ Villages/Server_StartGame.lua
@@ -1,15 +1,43 @@
 require('Utilities');
 require('WLUtilities');
 
-function Server_StartGame(game, addNewOrder)
+function Server_StartGame(game, standing)
+	local privateGameData = Mod.PrivateGameData
+	privateGameData.portals = {}
+	territoryArray = {}
 
-end -- end of function
-
-function getTableLength(t)
-	local a = 0;
-	for i, _ in pairs(t) do
-		
-		a = a + 1;
+	local count = 1
+	for _, territory in pairs(game.Map.Territories) do
+		territoryArray[count] = territory
+		count = count + 1
 	end
-	return a;
+
+	-- Check that the map has enough territories, else make the minimum number of portals
+	local NumPortals = Mod.Settings.NumPortals
+	if (#territoryArray < Mod.Settings.NumPortals * 2) then
+		NumPortals = 1
+	end
+
+	structure = {}
+	Portals = WL.StructureType.Power
+	structure[Portals] = 0
+
+	for i = 1, NumPortals * 2 do
+		privateGameData.portals[i] = getRandomTerritory(territoryArray)
+		if (i % 2 == 1) then
+			structure[Portals] = structure[Portals] + 1
+		end
+
+		standing.Territories[privateGameData.portals[i]].Structures = structure
+	end
+
+	Mod.PrivateGameData = privateGameData
+end
+
+function getRandomTerritory(territoryArray)
+	local index = math.random(#territoryArray)
+	local territoryID = territoryArray[index].ID
+	table.remove(territoryArray, index)
+
+	return territoryID
 end
