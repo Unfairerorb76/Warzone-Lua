f50a164972cd35d372db15b92568fe9803a2a981
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-07-29 14:07:21 +0100

    Update Server_StartGame.lua

--- Villages/Server_StartGame.lua
+++ Villages/Server_StartGame.lua
@@ -2,36 +2,6 @@ require('Utilities');
 require('WLUtilities');
 
 function Server_StartGame(game, addNewOrder)
-print(1);
-  local terr = {}; --table of neutral territories
-  local randomNeutralTerr; 	
-   
-	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-		table.insert(terr, terrID);   --gets each territory ID of neutrals
-			end
-	
-print(2);	
-amountOfVillages = Mod.Settings.NumOfVillages
-
-	for i = 1, amountOfVillages do
-		local rand = math.random(#terr);
-		local structures = standing.Territories[Terr[rand]].Structures;
-		if structures == nil then 
-			structures = {}; 
-			structures[structure] = 1;
-		else
-			if structures[structure] == nil then
-				structures[structure] = 1;
-			else
-				structures[structure] = structures[structure] + 1;
-			end
-		end
-print(3);
-		standing.Territories[terr[rand]].Structures = structures;
-		table.remove(terr, rand)
-	end
-	return structure;
-	
 
 end -- end of function
 
