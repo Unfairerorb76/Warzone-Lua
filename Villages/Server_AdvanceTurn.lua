95401f5182ad0b9904af9772685e1563038952f6
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-08-02 10:24:56 +0100

    Update Server_AdvanceTurn.lua

--- Villages/Server_AdvanceTurn.lua
+++ Villages/Server_AdvanceTurn.lua
@@ -2,28 +2,7 @@ require('Utilities');
 require('WLUtilities');
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-print(1);
-  local terr = {}; --table of neutral territories
-  local randomNeutralTerr; 	
-  local Villages = WL.StructureType.MercenaryCamp;
-	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-		table.insert(terr, terrID);   --gets each territory ID of neutrals
-			end
-	
-print(2);	
-amountOfVillages = Mod.Settings.NumOfVillages
 
-	for i = 1, amountOfVillages do
-		local rand = math.random(#terr);
-		local structures = 
-	        local randomNeutralTerr = terr[rand];
-		
-		local terrMod = WL.TerritoryModification.Create(randomNeutralTerr); 
-		end
-print(3);
-		standing.Territories[terr[rand]].Structures = structures;
-		table.remove(terr, rand)
-	return structure;
 	end
 	
 
