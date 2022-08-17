f6f3bc7ef413610ad0f475ec7698e90e23b09556
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-08-17 14:21:44 +0100

    Update Server_StartGame.lua

--- Villages/Server_StartGame.lua
+++ Villages/Server_StartGame.lua
@@ -8,7 +8,7 @@ function Server_StartGame(game, standing)
 
 	local count = 1
 	for _, territory in pairs(game.Map.Territories) do
-           if territoryArray[count].OwnerPlayerID == WL.PlayerID.Neutral then
+           if territoryArray.OwnerPlayerID == WL.PlayerID.Neutral then
 		territoryArray[count] = territory
 		count = count + 1
            end
