51598b96c138a5003237eccda91799430c2cf71c
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-15 17:45:47 +0100

    Update Client_PresentSettingsUI.lua

--- Villages/Client_PresentSettingsUI.lua
+++ Villages/Client_PresentSettingsUI.lua
@@ -3,7 +3,7 @@ function Client_PresentSettingsUI(rootParent)
 
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
 UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages);
-UI.CreateLabel(vert).SetText('villages claims only effect neutrals = ' .. Mod.Settings.ONeutrals);
+
 UI.CreateLabel(vert).SetText('amount of armies with each claimed territory = ' .. Mod.Settings.Armies);
 	
 end
