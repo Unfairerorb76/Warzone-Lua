35d34590ec36968dc34bf66860648c6471d5bda1
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-15 16:48:19 +0100

    Update Client_PresentConfigureUI.lua

--- Villages/Client_PresentConfigureUI.lua
+++ Villages/Client_PresentConfigureUI.lua
@@ -1,10 +1,19 @@
 function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 	print(1);
 	local initialVillages = Mod.Settings.NumOfVillages;
+	local OnlyNeutrals = Mod.Settings.ONeutrals;
+	local GainedArmies = Mod.Settings.Armies
 	
 	if initialVillages == nil then
 		initialVillages = 3;
 	end
+	if OnlyNeutrals == nil then
+		OnlyNeutrals == false;
+	end
+	if GainedArmies = nil then
+		GainedArmies = 2;
+	end
+		
 	
     
  local vert = UI.CreateVerticalLayoutGroup(rootParent);
@@ -16,5 +25,16 @@ function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(10)
 		.SetValue(initialVillages);
-       print(2); 
+	
+	UI.CreateLabel(vert).SetText('Amount of armies that you will get with each new territory');
+   	 numberInputField = UI.CreateNumberInputField(vert)
+		.SetSliderMinValue(1)
+		.SetSliderMaxValue(15)
+		.SetValue(GainedArmies);
+	
+	UI.CreateLabel(vert).SetText('only neutrals territories shall be claimed (recommended)');
+        booleanInputField = UI.CreateCheckBox(vert)        
+                .SetIsChecked(OnlyNeutrals);
+	
+	
 end
