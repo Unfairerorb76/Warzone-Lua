ea7866301de77876a1245a02b1cef0b585e16c5b
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-15 10:51:58 +0100

    Update Server_AdvanceTurn.lua

--- Villages/Server_AdvanceTurn.lua
+++ Villages/Server_AdvanceTurn.lua
@@ -9,7 +9,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
                 if attackedTerr.Structures ~= nil then 
                     if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p
 					for terrID, _ in pairs(game.Map.Territories[order.To].ConnectedTo) do
-		                  terrID.PlayerID = attackedTerr.order.PlayerID;
+                            game.Map.Territories[order.To].ConnectedTo = attackedTerr.order.PlayerID;
 						end
 end end end end end
 
