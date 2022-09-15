e870932e4222a4a3c1de57ee72f10ac875baae1f
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-15 10:42:46 +0100

    Update Server_AdvanceTurn.lua

--- Villages/Server_AdvanceTurn.lua
+++ Villages/Server_AdvanceTurn.lua
@@ -1,57 +1,46 @@
 require('Utilities');
 require('WLUtilities');
 
-function Server_AdvanceTurn_End(game, addNewOrder)
-     local camps = {};
-     local count = 0;
+function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
+      if order.proxyType == "GameOrderAttackTransfer" then 
+          if orderResult.IsAttack and orderResult.IsSuccessful then 
+          local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To]; 
+
+                if attackedTerr.Structures ~= nil then 
+                    if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p 
+		           attackedTerr.ConnectedTo = attackedTerr.order.PlayerID;			
+end end end end end
+
+
+
+
+--function Server_AdvanceTurn_End(game, addNewOrder)
+     -- local camps = {};
+     --local count = 0;
 --local structures = structures[WL.StructureID.MercenaryCamp];     
-	--local structures = game.ServerGame.LatestTurnStanding.Territories[order.To].Structures;
-      
-      
+	--local structures = game.ServerGame.LatestTurnStanding.Territories[order.To].Structures;   
 		
-     for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-	if territory.Structures ~= nil then
-	   local structures = territory.Structures;
+     --for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+	--if territory.Structures ~= nil then
+	   --local structures = territory.Structures;
 
-    		if (structures[WL.StructureType.MercenaryCamp] and territory.OwnerPlayerID ~= WL.PlayerID.Neutral) then
+    		--if (structures[WL.StructureType.MercenaryCamp] and territory.OwnerPlayerID ~= WL.PlayerID.Neutral) then
 			
-      			table.insert(camps, terrID);   --gets each territory ID of controlled camps
-                         print(1);
-			count = count + 1;
-                    end     
-		end
-	end
-     print(count);
-	print(4);
-     
-	for times = 1, math.min(count, math.floor(#camps / getTableLength(game.ServerGame.Game.PlayingPlayers))) do
-		print(10);
-	for i, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
-                 local rand = math.random(#camps);
-			
+      			--table.insert(camps, terrID);   --gets each territory ID of controlled camps
+                         
+			--count = count + 1;
+                    --end     
+		--end
+	--end
+   
+--for times = 1, math.min(count, math.floor(#camps / getTableLength(game.ServerGame.Game.PlayingPlayers))) do
 		
-                  print(2);                 
-                        end
-
-
-           
-
-     end 
-      end
-end
-
---function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
- ----     if order.proxyType == "GameOrderAttackTransfer" then 
-        --    if orderResult.IsAttack and orderResult.IsSuccessful then 
-        --    local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To]; 
-
-               -- if attackedTerr.Structures ~= nil then 
-                 --   if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p 
---end end end end end
-
-
-
-
+	--for i, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
+                 --local rand = math.random(#camps);               
+        --end
+	--end 
+      --end
+--end
 
 function getTableLength(t)
 	local a = 0;
