714c3cc2579fae962c71643d05c12727d224fc31
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-15 17:16:45 +0100

    Update Client_SaveConfigureUI.lua

--- Villages/Client_SaveConfigureUI.lua
+++ Villages/Client_SaveConfigureUI.lua
@@ -1,7 +1,7 @@
 function Client_SaveConfigureUI(alert)
     
     Mod.Settings.NumOfVillages = numberInputField.GetValue();
-    Mod.Settings.ONeutrals = booleanInputField.GetIsChecked();
     Mod.Settings.Armies = numberInputField2.GetValue();
+    Mod.Settings.ONeutrals = booleanInputField.GetIsChecked();
     
 end
