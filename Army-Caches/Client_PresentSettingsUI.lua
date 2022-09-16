b7586724a45a91170d2b98476b5582d458db46d9
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-16 09:53:48 +0100

    Update Client_PresentSettingsUI.lua

--- Army-Caches/Client_PresentSettingsUI.lua
+++ Army-Caches/Client_PresentSettingsUI.lua
@@ -2,8 +2,7 @@ function Client_PresentSettingsUI(rootParent)
 	
 
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
-UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages);
-
-UI.CreateLabel(vert).SetText('amount of armies with each claimed territory = ' .. Mod.Settings.Armies);
+UI.CreateLabel(vert).SetText('Amount of Army Caches that spawned = ' .. Mod.Settings.NumOfACaches);
+UI.CreateLabel(vert).SetText('Amount of armies with each claimed Cache = ' .. Mod.Settings.Armies);
 	
 end
