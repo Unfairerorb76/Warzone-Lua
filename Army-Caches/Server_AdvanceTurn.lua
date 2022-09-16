070a47b6e4a9aa8fcdaf8143410d88ae73fea33d
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-16 12:38:42 +0100

    Update Server_AdvanceTurn.lua

--- Army-Caches/Server_AdvanceTurn.lua
+++ Army-Caches/Server_AdvanceTurn.lua
@@ -7,25 +7,13 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
           local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To]; 
 
                 if attackedTerr.Structures ~= nil then 
-                    if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p
-					for terrID, territory in pairs(game.Map.Territories[order.To].ConnectedTo) do
-					  if (Mod.Settings.ONeutrals == true) then	
-					    if (game.ServerGame.LatestTurnStanding.Territories[terrID].IsNeutral == true) then
-						local terrMod = WL.TerritoryModification.Create(terrID);
-						terrMod.SetOwnerOpt = order.PlayerID;
+                    if attackedTerr.Structures[WL.StructureType.ArmyCache] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p
+					
+						local terrMod = WL.TerritoryModification.Create(attackedTerr);
 						terrMod.SetArmiesTo = Mod.Settings.Armies;
 						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,"new territory",{},{terrMod}), true);
 					    end
-					 end
-					 if (Mod.Settings.ONeutrals == false) then
-					     
-						local terrMod = WL.TerritoryModification.Create(terrID);
-						terrMod.SetOwnerOpt = order.PlayerID;
-						terrMod.SetArmiesTo = Mod.Settings.Armies;
-						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,"new territory",{},{terrMod}), true);			
-					   
-					end
-				     end				
+				        				
 end end end end end      
 
 function getTableLength(t)
