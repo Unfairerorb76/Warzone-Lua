ead37ba62b8101229077d966816247d96b05f16c
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-20 10:13:22 +0100

    Update Server_StartGame.lua

--- Villages/Server_StartGame.lua
+++ Villages/Server_StartGame.lua
@@ -1,7 +1,9 @@
 require('Utilities');
 require('WLUtilities');
-
+require('distributeRandomStructures');
 function Server_StartGame(game, standing)
+	
+distributeRandomStructures(standing, WL.StructureType.MercenaryCamp, 100, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
 	local privateGameData = Mod.PrivateGameData
 	privateGameData.portals = {}
 	territoryArray = {}
