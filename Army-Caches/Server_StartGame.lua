a0989d0fd4eb181ca7d667cda6875a07b25c38de
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-22 10:13:44 +0100

    Update Server_StartGame.lua

--- Army-Caches/Server_StartGame.lua
+++ Army-Caches/Server_StartGame.lua
@@ -1,40 +1,10 @@
 require('Utilities');
 require('WLUtilities');
+require('distributeRandomStructures');
 
 function Server_StartGame(game, standing)
-print(10);
-	local privateGameData = Mod.PrivateGameData
-	privateGameData.portals = {}
-	territoryArray = {}
-print(1);
-	local count = 1
-	--for _, territory in pairs(game.Map.Territories) do
-        for _, territory in pairs(standing.Territories) do
-      if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
-		territoryArray[count] = territory
-		count = count + 1
-          end 
-	end  -- territory array
-print(2);
-	-- Check that the map has enough territories, if not then it only creates one village
-	local NumOfACaches = Mod.Settings.NumOfACaches  
-	if (#territoryArray < Mod.Settings.NumOfACaches) then
-		NumOfACaches = 1 
-	end
-print(3);
-	structure = {}
-	ArmyCaches = WL.StructureType.ArmyCache
-	structure[ArmyCaches] = 0
 
-	for i = 1, NumOfACaches do
-		privateGameData.portals[i] = getRandomTerritory(territoryArray)
-		
-			structure[ArmyCaches] = 1
-
-		standing.Territories[privateGameData.portals[i]].Structures = structure
-	end
-print(4);
-	Mod.PrivateGameData = privateGameData
+distributeRandomStructures(standing, WL.StructureType.ArmyCamp, 100, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false, allowConnectedTerrs = false, mapDetails = game.Map});
 end
 
 function getRandomTerritory(territoryArray)
