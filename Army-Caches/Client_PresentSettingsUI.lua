4602d8b70055daff34b9f8798ef02fd2f0ef6d7c
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-22 13:07:47 +0100

    Update Client_PresentSettingsUI.lua

--- Army-Caches/Client_PresentSettingsUI.lua
+++ Army-Caches/Client_PresentSettingsUI.lua
@@ -4,5 +4,7 @@ function Client_PresentSettingsUI(rootParent)
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
 UI.CreateLabel(vert).SetText('Amount of Army Caches that spawned = ' .. Mod.Settings.NumOfACaches);
 UI.CreateLabel(vert).SetText('Amount of armies with each claimed Cache = ' .. Mod.Settings.Armies);
-	
+if Mod.Settings.FixedArmies == true then
+UI.CreateLabel(vert).SetText('Random +/- limit of:' Mod.Settings.luck);
+end	
 end
