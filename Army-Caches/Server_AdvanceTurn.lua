af6d738f8833a37342da62175a9fab6406fa68ee
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-22 13:22:36 +0100

    Update Server_AdvanceTurn.lua

--- Army-Caches/Server_AdvanceTurn.lua
+++ Army-Caches/Server_AdvanceTurn.lua
@@ -9,17 +9,28 @@ local ArmyAmount = Mod.Settings.Armies;
           local structures = TransferredTerr.Structures
                 if TransferredTerr.Structures ~= nil then 
                     if TransferredTerr.Structures[WL.StructureType.ArmyCache] ~= nil then -- there is a army cache on the territory that was successfully attacked -- so now you can do what you want :p
-						 
-					        local ArmyAmount = Mod.Settings.Armies;
+					if Mod.Settings.FixedArmies == true then	 
+					        local IncomeAmount = Mod.Settings.Armies;
 					        local terrMod = WL.TerritoryModification.Create(order.To);
 					
 						structures = {}
 						structures[WL.StructureType.ArmyCache] = -1;					
 					        terrMod.AddStructuresOpt = structures;
 					
-						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(order.PlayerID, ArmyAmount, "You have captured an army cache")}));	
-					--addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,"new territory",{},{terrMod}), true);
-					 
+						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(order.PlayerID, IncomeAmount, "You have captured an army cache")}));	
+					 end
+					if Mod.Settings.FixedArmies == false then
+						
+						local IncomeAmount = Mod.Settings.Armies;
+						IncomeAmount = math.random(-Mod.Settings.Luck, Mod.Settings.Luck);
+					        local terrMod = WL.TerritoryModification.Create(order.To);
+					
+						structures = {}
+						structures[WL.StructureType.ArmyCache] = -1;					
+					        terrMod.AddStructuresOpt = structures;
+					
+						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(order.PlayerID, IncomeAmount, "You have captured an army cache")}));	
+					 end
 				        				
 end end end end end   
 
