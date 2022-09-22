74fec78a9b8245e13bb636561f8cedde2000c3e6
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-22 13:09:12 +0100

    Update Client_SaveConfigureUI.lua

--- Army-Caches/Client_SaveConfigureUI.lua
+++ Army-Caches/Client_SaveConfigureUI.lua
@@ -2,5 +2,7 @@ function Client_SaveConfigureUI(alert)
     
     Mod.Settings.NumOfACaches = numberInputField.GetValue();
     Mod.Settings.Armies = numberInputField2.GetValue();
+    Mod.Settings.FixedArmies = booleanInputField.GetIsChecked();
+    Mod.Settings.Luck = numberInputField3.GetValue();
     
 end
