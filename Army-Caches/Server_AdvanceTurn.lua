8d0bdb141c3d5e4db3133fb956be608c7ef3c004
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-09-22 12:26:07 +0100

    Update Server_AdvanceTurn.lua

--- Army-Caches/Server_AdvanceTurn.lua
+++ Army-Caches/Server_AdvanceTurn.lua
@@ -10,10 +10,12 @@ local ArmyAmount = Mod.Settings.Armies;
                 if TransferredTerr.Structures ~= nil then 
                     if TransferredTerr.Structures[WL.StructureType.ArmyCache] ~= nil then -- there is a army cache on the territory that was successfully attacked -- so now you can do what you want :p
 						       
+					        local terrMod = WL.TerritoryModification.Create(order.To);
+					
 					       	structures[WL.StructureType.ArmyCache] = structures[WL.StructureType.ArmyCache] - 1;					
 					
 						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Updated income", {}, {}, {}, {WL.IncomeMod.Create(order.PlayerID, ArmyAmount, "You have captured an army cache")}));
-					        addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Destroyed a fort', {}, {}));
+					        addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Destroyed a fort', {}, {terrMod}));
 						--addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,"new territory",{},{terrMod}), true);
 					 
 				        				
