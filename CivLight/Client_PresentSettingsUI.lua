c8ffb4acebfb2fcf7d18568ab0820545a6386e86
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-25 16:04:58 +0100

    Update Client_PresentSettingsUI.lua

--- CivLight/Client_PresentSettingsUI.lua
+++ CivLight/Client_PresentSettingsUI.lua
@@ -3,5 +3,10 @@ function Client_PresentSettingsUI(rootParent)
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
 UI.CreateLabel(vert).SetText('amount of territories(neutrals) you get for free every turn = ' .. Mod.Settings.NumToConvert);
 UI.CreateLabel(vert).SetText('amount of armies that come with each new territory = ' .. Mod.Settings.SetArmiesTo);
+  
+UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages);
+UI.CreateLabel(vert).SetText('amount of armies with each claimed territory = ' .. Mod.Settings.Armies);
+  
+
 
 end
