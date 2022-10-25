1fd1d76b43004e043489abbe01d25c6aa8c0b0f7
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-25 16:10:57 +0100

    Update Server_AdvanceTurn.lua

--- CivLight/Server_AdvanceTurn.lua
+++ CivLight/Server_AdvanceTurn.lua
@@ -1,3 +1,45 @@
+require('Utilities');
+require('WLUtilities');
+
+function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
+      if order.proxyType == "GameOrderAttackTransfer" then 
+          if orderResult.IsAttack and orderResult.IsSuccessful then 
+          local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To]; 
+
+                if attackedTerr.Structures ~= nil then 
+                    if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p
+					
+					local list = {};
+					
+					for terrID, territory in pairs(game.Map.Territories[order.To].ConnectedTo) do
+					  if (Mod.Settings.ONeutrals == true) then	
+					    if (game.ServerGame.LatestTurnStanding.Territories[terrID].IsNeutral == true) then
+						local terrMod = WL.TerritoryModification.Create(terrID);
+						terrMod.SetOwnerOpt = order.PlayerID;
+						terrMod.SetArmiesTo = Mod.Settings.Armies;
+						table.insert(list, terrMod);
+					    end
+					 end
+					 if (Mod.Settings.ONeutrals == false) then
+					     
+						local terrMod = WL.TerritoryModification.Create(terrID);
+						 if (game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID ~= order.PlayerID) then
+						terrMod.SetOwnerOpt = order.PlayerID;
+						terrMod.SetArmiesTo = Mod.Settings.Armies;
+						if terrMod ~= nil then
+						table.insert(list, terrMod);
+						end
+						end
+								
+					   
+					end
+				
+				     end	
+				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,"new territory",{}, list), true);
+end end end end end      
+
+
+
 function Server_AdvanceTurn_End(game, addNewOrder)
    	
 	local pTable = {}; -- table of player territories
