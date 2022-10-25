c1bb155e157598dd0fe6afc091a2c97639735756
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-25 16:29:29 +0100

    Update Server_StartGame.lua

--- Army-Caches/Server_StartGame.lua
+++ Army-Caches/Server_StartGame.lua
@@ -7,11 +7,11 @@ function Server_StartGame(game, standing)
 distributeRandomStructures(standing, WL.StructureType.ArmyCache, Mod.Settings.NumOfACaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
 end
 
-function getRandomTerritory(territoryArray)
-	local index = math.random(#territoryArray)
-	local territoryID = territoryArray[index].ID
-	table.remove(territoryArray, index)
+--function getRandomTerritory(territoryArray)
+--	local index = math.random(#territoryArray)
+--	local territoryID = territoryArray[index].ID
+--	table.remove(territoryArray, index)
 
-	return territoryID
-end
+--	return territoryID
+--end
 
