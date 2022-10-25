33d8b9c1242cc7b283bc4e9849b07e67ceaf0abb
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-25 16:29:23 +0100

    Update Server_StartGame.lua

--- CivLight/Server_StartGame.lua
+++ CivLight/Server_StartGame.lua
@@ -1,8 +1,11 @@
 require('Utilities');
 require('WLUtilities');
 require('distributeRandomStructures');
+require('distributeRandomStructures2');
 
 function Server_StartGame(game, standing)
 	
 distributeRandomStructures(standing, WL.StructureType.MercenaryCamp, 100, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false, allowConnectedTerrs = false, mapDetails = game.Map});
+distributeRandomStructures2(standing, WL.StructureType.ArmyCache, Mod.Settings.NumOfACaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
+
 end
