b337884304f4e6bdd89dea9b2d5bfab7ffa96c28
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-26 09:29:20 +0100

    Update Server_AdvanceTurn.lua

--- CivLight/Server_AdvanceTurn.lua
+++ CivLight/Server_AdvanceTurn.lua
@@ -8,8 +8,8 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
             print(1);
                 if attackedTerr.Structures ~= nil then 
                     if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p
-                                        local place = game.Map.Territories[order.to].ConnectedTo
-					Village(Game, addNewOrder, terrID, place);
+                                        
+					Village(Game, addNewOrder, terrID, order);
 					end
 				
 				    
@@ -171,10 +171,10 @@ end
 
 end
 
-function Village(Game, addNewOrder, terrID, place)
+function Village(Game, addNewOrder, terrID, order)
 					local list = {};
 					
-					for terrID, territory in pairs(place) do
+					for terrID, territory in pairs(game.Map.Territories[order.to].ConnectedTo) do
 					  if (Mod.Settings.ONeutrals == true) then	
 					    if (game.ServerGame.LatestTurnStanding.Territories[terrID].IsNeutral == true) then
 						local terrMod = WL.TerritoryModification.Create(terrID);
