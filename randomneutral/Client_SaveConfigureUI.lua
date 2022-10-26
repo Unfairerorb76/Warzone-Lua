098c187943c0403f2951ce668d4497ce18e6e1c5
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-26 14:33:27 +0100

    Delete randomneutral directory

--- randomneutral/Client_SaveConfigureUI.lua
+++
@@ -1,8 +0,0 @@
-
-function Client_SaveConfigureUI(alert)
-    
-    Mod.Settings.NumToConvert = numberInputField.GetValue();
-    Mod.Settings.SetArmiesTo = numberInputField2.GetValue();
-    Mod.Settings.OnlyBaseNeutrals = booleanInputField.GetIsChecked();
-  print(Mod.Settings.OnlyBaseNeutrals);
-end
