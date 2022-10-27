d678fa7e3ddfe513d19fc8ca545629f5f2487426
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-27 17:21:20 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- CivLight/Client_PresentCommercePurchaseUI.lua
+++ CivLight/Client_PresentCommercePurchaseUI.lua
@@ -1,3 +1,5 @@
 function Client_PresentCommercePurchaseUI(rootParent, game, close)
+     
+    local vert = UI.CreateVerticalLayoutGroup(rootParent);
      UI.CreateLabel(vert).SetText("Everyday im shuffling");
 end
