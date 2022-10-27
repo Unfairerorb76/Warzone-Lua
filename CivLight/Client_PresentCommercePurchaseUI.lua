3e3dcbbac4d68dac7cd3d0cac946450916ee8fb7
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-27 17:21:53 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- CivLight/Client_PresentCommercePurchaseUI.lua
+++ CivLight/Client_PresentCommercePurchaseUI.lua
@@ -1,5 +1,5 @@
 function Client_PresentCommercePurchaseUI(rootParent, game, close)
      
     local vert = UI.CreateVerticalLayoutGroup(rootParent);
-     UI.CreateLabel(vert).SetText("Everyday im shuffling");
+     UI.CreateLabel(vert).SetText("Im never gonna give u up, never gonna let u down, never gonna turn u around..");
 end
