d0d9b24109cb8668d3b4eefcff403465dfc7b327
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-28 14:32:41 +0100

    Update Client_PresentConfigureUI.lua

--- FreeExpansion+/Client_PresentConfigureUI.lua
+++ FreeExpansion+/Client_PresentConfigureUI.lua
@@ -1,41 +1,41 @@
 function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 	
 	print(1);
-	local initialValueConvert = Mod.Settings.NumToConvert;
-	local initialValueArmies = Mod.Settings.SetArmiesTo;
-        local initalcheckbox = Mod.Settings.OnlyBaseNeutrals;
+	--local initialValueConvert = Mod.Settings.NumToConvert;
+	--local initialValueArmies = Mod.Settings.SetArmiesTo;
+        --local initalcheckbox = Mod.Settings.OnlyBaseNeutrals;
 	
-	if initialValueConvert == nil then
-		initialValueConvert = 2;
-	end
+	--if initialValueConvert == nil then
+	--	initialValueConvert = 2;
+	--end
 	
-	if initialValueArmies == nil then
-		initialValueArmies = 2;
-	end
+	--if initialValueArmies == nil then
+	--	initialValueArmies = 2;
+	--end
 
-        if initalcheckbox == nil then 
-                initalcheckbox = false; 
-        end
+     --   if initalcheckbox == nil then 
+       --         initalcheckbox = false; 
+        --end
        print(2);
- local vert = UI.CreateVerticalLayoutGroup(rootParent);
-local horz1 = UI.CreateHorizontalLayoutGroup(vert);  --not used
-local horz2 = UI.CreateHorizontalLayoutGroup(vert);  --not used but here for reference
+ --local vert = UI.CreateVerticalLayoutGroup(rootParent);
+--local horz1 = UI.CreateHorizontalLayoutGroup(vert);  --not used
+--local horz2 = UI.CreateHorizontalLayoutGroup(vert);  --not used but here for reference
 	print(3);
-	UI.CreateLabel(vert).SetText('only base neutral armies and less shall be claimed');
-        booleanInputField = UI.CreateCheckBox(vert)        
-                .SetIsChecked(initalcheckbox);
+	--UI.CreateLabel(vert).SetText('only base neutral armies and less shall be claimed');
+        --booleanInputField = UI.CreateCheckBox(vert)        
+        --        .SetIsChecked(initalcheckbox);
 	print(4);
-	UI.CreateLabel(vert).SetText('amount of neutrals a player shall gain each turn');
-    numberInputField = UI.CreateNumberInputField(vert)
-		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(initialValueConvert);
+	--UI.CreateLabel(vert).SetText('amount of neutrals a player shall gain each turn');
+    --numberInputField = UI.CreateNumberInputField(vert)
+	--	.SetSliderMinValue(1)
+	--	.SetSliderMaxValue(10)
+	--	.SetValue(initialValueConvert);
 	print(5);
-	 UI.CreateLabel(vert).SetText('amount of armies a player shall get with the territory');
-    numberInputField2 = UI.CreateNumberInputField(vert)
-		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(initialValueArmies); 
+	 --UI.CreateLabel(vert).SetText('amount of armies a player shall get with the territory');
+    --numberInputField2 = UI.CreateNumberInputField(vert)
+	--	.SetSliderMinValue(1)
+	--	.SetSliderMaxValue(10)
+	--	.SetValue(initialValueArmies); 
  
         
 end
