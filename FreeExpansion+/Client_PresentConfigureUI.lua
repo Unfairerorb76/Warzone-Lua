786ce85ad1f3d110fbcb9a2e8e566ac3913b107a
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-10-28 14:21:42 +0100

    Update Client_PresentConfigureUI.lua

--- FreeExpansion+/Client_PresentConfigureUI.lua
+++ FreeExpansion+/Client_PresentConfigureUI.lua
@@ -1,4 +1,4 @@
-function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
+function Client_PresentConfigureUI(rootParent)
 	
 	print(1);
 	local initialValueConvert = Mod.Settings.NumToConvert;
