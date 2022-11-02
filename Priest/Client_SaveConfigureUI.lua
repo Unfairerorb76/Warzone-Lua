f2ebc41719b79edde11d2a6d073a6712d253cde0
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-02 12:22:00 +0000

    Update Client_SaveConfigureUI.lua

--- Priest/Client_SaveConfigureUI.lua
+++ Priest/Client_SaveConfigureUI.lua
@@ -1,13 +1,13 @@
 function Client_SaveConfigureUI(alert)
     local cost = costInputField.GetValue();
-    if cost < 1 then alert("Cost to buy a tank must be positive"); end
-    Mod.Settings.CostToBuyTank = cost;
+    if cost < 1 then alert("Cost to buy a priest must be positive"); end
+    Mod.Settings.CostToBuyPriest = cost;
 
     local power = powerInputField.GetValue();
-    if power < 1 then alert("Tank must have at least one power"); end
-    Mod.Settings.TankPower = power;
+    if power < 1 then alert("Priest must have at least one power"); end
+    Mod.Settings.PriestPower = power;
 
-    local maxTanks = maxTanksField.GetValue();
-    if maxTanks < 1 or maxTanks > 5 then alert("Max tanks must be between 1 and 5"); end
-    Mod.Settings.MaxTanks = maxTanks;
+    local maxPriests = maxPriestsField.GetValue();
+    if maxPriests < 1 or maxPriests > 5 then alert("Max priests must be between 1 and 5"); end
+    Mod.Settings.MaxPriests = maxPriests;
 end
