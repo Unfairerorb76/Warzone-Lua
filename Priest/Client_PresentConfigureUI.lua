1e7a9af3e86798d6e29f41360535ec2f89bb2ee6
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-02 12:19:32 +0000

    Update Client_PresentConfigureUI.lua

--- Priest/Client_PresentConfigureUI.lua
+++ Priest/Client_PresentConfigureUI.lua
@@ -7,19 +7,19 @@ function Client_PresentConfigureUI(rootParent)
 	end
 
 
-	local power = Mod.Settings.TankPower;
-	if power == nil then power = 10; end
+	local power = Mod.Settings.PriestPower;
+	if power == nil then power = 0; end
 
-	local cost = Mod.Settings.CostToBuyTank;
-	if cost == nil then cost = 20; end
+	local cost = Mod.Settings.CostToBuyPriest;
+	if cost == nil then cost = 25; end
 
-	local maxTanks = Mod.Settings.MaxTanks;
-	if maxTanks == nil then maxTanks = 2; end;
+	local maxPriests = Mod.Settings.MaxPriests;
+	if maxPriests == nil then maxPriests = 3; end;
     
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
     local row1 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row1).SetText('How much gold it costs to buy a tank');
+	UI.CreateLabel(row1).SetText('How much gold it costs to buy a priest');
     costInputField = UI.CreateNumberInputField(row1)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(40)
@@ -27,17 +27,17 @@ function Client_PresentConfigureUI(rootParent)
 
 
 	local row2 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row2).SetText('How powerful the tank is (in armies)');
+	UI.CreateLabel(row2).SetText('How powerful the priest is (in armies). (reccomended to be 0)');
 	powerInputField = UI.CreateNumberInputField(row2)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(30)
 		.SetValue(power);
 
 	local row3 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row3).SetText('How many tanks each player can have at a time');
-	maxTanksField = UI.CreateNumberInputField(row3)
+	UI.CreateLabel(row3).SetText('How many priests each player can have at a time');
+	maxPriestsField = UI.CreateNumberInputField(row3)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(5)
-		.SetValue(maxTanks);
+		.SetValue(maxPriests);
 	
 end
