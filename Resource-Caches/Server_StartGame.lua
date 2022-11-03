32e4ea57e57106547d9e21ce278a28cbf768b2ef
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-03 19:15:50 +0000

    Update Server_StartGame.lua

--- Resource-Caches/Server_StartGame.lua
+++ Resource-Caches/Server_StartGame.lua
@@ -6,48 +6,48 @@ function Server_StartGame(game, standing)
 
 distributeRandomStructures(standing, WL.StructureType.ResourceCache, Mod.Settings.NumOfRCaches, {maxPercentage = 50, numberOfStructures = 1, onlyPlaceOnNeutrals = true, allowMultipleStructures = false});
   	
-			local s = standing;
-			local cards = s.Cards;
-			local armies = 0;
+			--local s = standing;
+			--local cards = s.Cards;
+			--local armies = 0;
 	
-			if game.Settings.Cards[WL.CardID.Reinforcement] ~= nil then
-				if game.Settings.Cards[WL.CardID.Reinforcement].Mode == WL.ReinforcementCardMode.Fixed then
-					armies = game.Settings.Cards[WL.CardID.Reinforcement].FixedArmies;		-- fixed amount
-				elseif game.Settings.Cards[WL.CardID.Reinforcement].Mode == WL.ReinforcementCardMode.ProgressiveByNumberOfTerritories then
-					armies = round(getTableLength(game.ServerGame.Game.PlayingPlayers) * game.Settings.Cards[WL.CardID.Reinforcement].ProgressivePercentage)
-				else
-					armies = 1;			-- Turn 0, so 0 * x		To give some armies it is always 1
-				end
-			end
-			for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
-				local playerCards = WL.PlayerCards.Create(player.ID);
-				local newPieces = playerCards.Pieces;
-				local newCards = playerCards.WholeCards;
-				for card, cardGame in pairs(game.Settings.Cards) do
-					local totalPieces = cardGame.InitialPieces;
-					if Mod.Settings.cPieces ~= nil then
-						totalPieces = totalPieces + Mod.Settings.cPieces;
-				end
-					if card ~= WL.CardID.Reinforcement then
-						for k = 1, math.floor(totalPieces / cardGame.NumPieces) do
-							local instance = WL.NoParameterCardInstance.Create(card);
-							newCards[instance.ID] = instance;
-							print(instance);
-						end
-						newPieces[card] = totalPieces % cardGame.NumPieces;
-					else
-						for k = 1, math.floor(totalPieces / cardGame.NumPieces) do
-							local instance = WL.ReinforcementCardInstance.Create(armies);
-							newCards[instance.ID] = instance;
-						end
-						newPieces[card] = totalPieces % cardGame.NumPieces;
-					end
-				end
-				playerCards.WholeCards = newCards;
-				playerCards.Pieces = newPieces;
-				cards[player.ID] = playerCards;
-			end
-			s.Cards = cards;
-			standing = s;
+			--if game.Settings.Cards[WL.CardID.Reinforcement] ~= nil then
+--				if game.Settings.Cards[WL.CardID.Reinforcement].Mode == WL.ReinforcementCardMode.Fixed then
+--					armies = game.Settings.Cards[WL.CardID.Reinforcement].FixedArmies;		-- fixed amount
+--				elseif game.Settings.Cards[WL.CardID.Reinforcement].Mode == WL.ReinforcementCardMode.ProgressiveByNumberOfTerritories then
+--					armies = round(getTableLength(game.ServerGame.Game.PlayingPlayers) * game.Settings.Cards[WL.CardID.Reinforcement].ProgressivePercentage)
+--				else
+--					armies = 1;			-- Turn 0, so 0 * x		To give some armies it is always 1
+--				end
+--			end
+--			for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
+--				local playerCards = WL.PlayerCards.Create(player.ID);
+--				local newPieces = playerCards.Pieces;
+--				local newCards = playerCards.WholeCards;
+--				for card, cardGame in pairs(game.Settings.Cards) do
+--					local totalPieces = cardGame.InitialPieces;
+--					if Mod.Settings.cPieces ~= nil then
+--						totalPieces = totalPieces + Mod.Settings.cPieces;
+--				end
+--					if card ~= WL.CardID.Reinforcement then
+--						for k = 1, math.floor(totalPieces / cardGame.NumPieces) do
+--							local instance = WL.NoParameterCardInstance.Create(card);
+--							newCards[instance.ID] = instance;
+--							print(instance);
+--						end
+--						newPieces[card] = totalPieces % cardGame.NumPieces;
+--					else
+--						for k = 1, math.floor(totalPieces / cardGame.NumPieces) do
+--							local instance = WL.ReinforcementCardInstance.Create(armies);
+--							newCards[instance.ID] = instance;
+--						end
+--						newPieces[card] = totalPieces % cardGame.NumPieces;
+--					end
+--				end
+--				playerCards.WholeCards = newCards;
+--				playerCards.Pieces = newPieces;
+--				cards[player.ID] = playerCards;
+--			end
+--			s.Cards = cards;
+--			standing = s;
 					
 end
