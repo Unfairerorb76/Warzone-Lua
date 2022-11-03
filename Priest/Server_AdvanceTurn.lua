393ad17e366a53ac648dd02856eb0778b7e53f30
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-03 11:29:35 +0000

    Update Server_AdvanceTurn.lua

--- Priest/Server_AdvanceTurn.lua
+++ Priest/Server_AdvanceTurn.lua
@@ -57,8 +57,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 	end
      if order.proxyType == "GameOrderAttackTransfer" then 
           if orderResult.IsAttack and hasNoPriest(game.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies)
-		local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To];
-		local fromTerr = game.ServerGame.LatestTurnStanding.Territories[order.From];
+
 			local terrMod = WL.TerritoryModification.Create(order.From);
 			local p;
 			terrMod.AddArmies = orderResult.AttackingArmiesKilled.NumArmies
