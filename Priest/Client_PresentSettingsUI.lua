6896832d22a6abfc4883d97b985f7f67a62a8221
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-03 11:49:22 +0000

    Update Client_PresentSettingsUI.lua

--- Priest/Client_PresentSettingsUI.lua
+++ Priest/Client_PresentSettingsUI.lua
@@ -4,4 +4,5 @@ function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(vert).SetText('Priest cost: ' .. Mod.Settings.CostToBuyPriest);
 	UI.CreateLabel(vert).SetText('Priest power: ' .. Mod.Settings.PriestPower);
 	UI.CreateLabel(vert).SetText('Max priests: ' .. Mod.Settings.MaxPriests);
+	UI.CreateLabel(vert).SetText('conversion percentage: ' .. Mod.Settings.Percentage .. '%');
 end
