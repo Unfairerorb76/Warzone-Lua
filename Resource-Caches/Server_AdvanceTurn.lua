b706f26c72abe76533fb2813f670d144363a1b4b
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-03 19:26:27 +0000

    Update Server_AdvanceTurn.lua

--- Resource-Caches/Server_AdvanceTurn.lua
+++ Resource-Caches/Server_AdvanceTurn.lua
@@ -49,7 +49,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 					
 			local order = WL.GameOrderEvent.Create(player.ID, "adjusted pieces", {}, {}, {}, {});
 			local t = {};
-			t[player.ID] = newPieces;
+			t[player.ID] = newCards;
 			order.AddCardPiecesOpt = t;	
 			addNewOrder(order);
 
