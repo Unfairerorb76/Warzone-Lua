9e9e1422851e2d49153ae1cae67a51aafa3a8568
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-03 17:03:02 +0000

    Update Server_AdvanceTurn.lua

--- Resource-Caches/Server_AdvanceTurn.lua
+++ Resource-Caches/Server_AdvanceTurn.lua
@@ -8,32 +8,8 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
           local TransferredTerr = game.ServerGame.LatestTurnStanding.Territories[order.To]; 
           local structures = TransferredTerr.Structures
                 if TransferredTerr.Structures ~= nil then 
-                    if TransferredTerr.Structures[WL.StructureType.ArmyCache] ~= nil then -- there is a army cache on the territory that was successfully attacked -- so now you can do what you want :p
-					if Mod.Settings.FixedArmies == true then	 
-					        local IncomeAmount = Mod.Settings.Armies;
-					        local terrMod = WL.TerritoryModification.Create(order.To);
-					
-						structures = {}
-						structures[WL.StructureType.ArmyCache] = -1;					
-					        terrMod.AddStructuresOpt = structures;
-					
-						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(order.PlayerID, IncomeAmount, "You have captured an army cache")}));	
-					 end
-					if Mod.Settings.FixedArmies == false then
+                    if TransferredTerr.Structures[WL.StructureType.ResourceCache] ~= nil then -- there is a army cache on the territory that was successfully attacked -- so now you can do what you want :p
 						
-						local IncomeAmount = Mod.Settings.Armies;
-						print(IncomeAmount);
-						IncomeAmount = (IncomeAmount + math.random(-Mod.Settings.Luck, Mod.Settings.Luck));
-
-					        local terrMod = WL.TerritoryModification.Create(order.To);
-					
-						structures = {}
-						structures[WL.StructureType.ArmyCache] = -1;					
-					        terrMod.AddStructuresOpt = structures;
-					
-						addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(order.PlayerID, IncomeAmount, "You have captured an army cache")}));	
-					 end
-				        				
 end end end end end   
 
 function getTableLength(t)
