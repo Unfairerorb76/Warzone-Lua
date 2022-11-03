7bf48b4fad8c56b550b73f5babebbc90fde01a9d
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-03 16:56:09 +0000

    Update Client_PresentConfigureUI.lua

--- Resource-Caches/Client_PresentConfigureUI.lua
+++ Resource-Caches/Client_PresentConfigureUI.lua
@@ -1,50 +1,32 @@
 function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 	print(1);
-	local initialACaches = Mod.Settings.NumOfACaches;
-	local GainedArmies = Mod.Settings.Armies;
-	local FixedArmies = Mod.Settings.FixedArmies;
+	local initialRCaches = Mod.Settings.NumOfRCaches;
+	local Pieces = Mod.Settings.cPieces
+	
 	local difference = Mod.Settings.Luck;
 	
-	if initialACaches == nil then
-		initialACaches = 2;
+	if initialRCaches == nil then
+		initialRCaches = 2;
 	end
 
-	if GainedArmies == nil then
-		GainedArmies = 5;
-	end
-	
-	if FixedArmies == nil then
-		FixedArmies = true;
-	end
-	
-	if difference == nil then 
-		difference = 5;
+	if Pieces == nil then
+		Pieces = 5;
 	end
     
  local vert = UI.CreateVerticalLayoutGroup(rootParent);
  local horz1 = UI.CreateHorizontalLayoutGroup(vert);  --not used
  local horz2 = UI.CreateHorizontalLayoutGroup(vert);  --not used but here for reference
 	
-		UI.CreateLabel(vert).SetText('Amount of Army Caches that will spawn at the start of the game');
+		UI.CreateLabel(vert).SetText('Amount of Resource Caches that will spawn at the start of the game');
    	 numberInputField = UI.CreateNumberInputField(vert)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(20)
-		.SetValue(initialACaches);
+		.SetValue(initialRCaches);
 	
-	UI.CreateLabel(vert).SetText('Amount of armies that you will get for claiming a cache');
+	UI.CreateLabel(vert).SetText('Amount of card pieces you will get for claiming a cache');
    	 numberInputField2 = UI.CreateNumberInputField(vert)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(15)
-		.SetValue(GainedArmies);
-	
-	UI.CreateLabel(vert).SetText('if checked will only give a fixed amount of armies');
-        booleanInputField = UI.CreateCheckBox(vert)        
-                .SetIsChecked(FixedArmies);
-	
-	UI.CreateLabel(vert).SetText('Random +/- limit');
-    numberInputField3 = UI.CreateNumberInputField(vert)
-		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(difference);
+		.SetValue(Pieces);
 	
 end
