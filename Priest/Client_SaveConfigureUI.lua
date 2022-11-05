557058a5afa9659f381eb14c9929a051e21a08a3
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-05 18:36:11 +0000

    Update Client_SaveConfigureUI.lua

--- Priest/Client_SaveConfigureUI.lua
+++ Priest/Client_SaveConfigureUI.lua
@@ -1,14 +1,14 @@
 function Client_SaveConfigureUI(alert)
     local cost = costInputField.GetValue();
-    if cost < 1 then alert("Cost to buy a priest must be positive"); end
+    if cost < 1 then alert("the cost to buy a priest must be positive"); end
     Mod.Settings.CostToBuyPriest = cost;
 
     local power = powerInputField.GetValue();
-    if power < 1 then alert("Priest must have at least one power"); end
+    if power < 1 then alert("a priest unit must have at least one power"); end
     Mod.Settings.PriestPower = power;
 
     local maxPriests = maxPriestsField.GetValue();
-    if maxPriests < 1 or maxPriests > 5 then alert("Max priests must be between 1 and 5"); end
+    if maxPriests < 1 or maxPriests > 5 then alert("Max number of priests per player must be between 1 and 5"); end
     Mod.Settings.MaxPriests = maxPriests;
     
     local percentage = percentageField.GetValue();
