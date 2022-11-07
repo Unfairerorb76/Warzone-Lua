4e02da9fc3a69e5ef53ec2b6bff5b5881392021b
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-07 17:25:57 +0000

    Update Server_AdvanceTurn.lua

--- Resource-Caches/Server_AdvanceTurn.lua
+++ Resource-Caches/Server_AdvanceTurn.lua
@@ -19,7 +19,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
                             local pieces = 1;
                             local rand = math.random(#cardArray);
                             local randomCard = cardArray[rand]; --picks random card to give to player
-                            local playerCards = game.ServerGame.LatestTurnStanding.Cards[order.PlayerID];
+                            local playerCards = game.ServerGame.LatestTurnStanding.Cards.Pieces[randomCard];
 			 		
 						
 			    local terrMod = WL.TerritoryModification.Create(order.To);
@@ -30,7 +30,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 			    
 			local cardEvent = WL.GameOrderEvent.Create(order.PlayerID, "Updated cards", {}, {terrMod}, {}, {})
 			local t = {};
-			t[order.PlayerID] = playerCards[randomCard];
+			t[order.PlayerID] = playerCards;
 			cardEvent.AddCardPiecesOpt = t;
 			addNewOrder(cardEvent);
                         end
