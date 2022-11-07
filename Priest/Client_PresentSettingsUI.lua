1f1bf40fd05a044b410e18f6595ec08e27d7c916
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-07 11:27:44 +0000

    Update Client_PresentSettingsUI.lua

--- Priest/Client_PresentSettingsUI.lua
+++ Priest/Client_PresentSettingsUI.lua
@@ -5,4 +5,11 @@ function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(vert).SetText('power of a priest unit: ' .. Mod.Settings.PriestPower);
 	UI.CreateLabel(vert).SetText('Max priests: ' .. Mod.Settings.MaxPriests);
 	UI.CreateLabel(vert).SetText('conversion percentage: ' .. Mod.Settings.Percentage .. '%');
+	
+	if Mod.Settings.Offensive then
+		UI.CreateLabel(vert).SetText('Priest converts when he attacks);
+	end
+	if Mod.Settings.Defensive then
+		UI.CreateLabel(vert).SetText('Priest converts when he is attacked);
+	end
 end
