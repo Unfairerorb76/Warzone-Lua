85ce00d42384f1aae7699fe0f80c34cee916be19
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-08 18:33:12 +0000

    Update Client_PresentSettingsUI.lua

--- Resource-Caches/Client_PresentSettingsUI.lua
+++ Resource-Caches/Client_PresentSettingsUI.lua
@@ -3,7 +3,7 @@ function Client_PresentSettingsUI(rootParent)
 local vert = UI.CreateVerticalLayoutGroup(rootParent);	
 UI.CreateLabel(vert).SetText('Amount of Resource Caches that spawned = ' .. Mod.Settings.NumOfACaches);
 UI.CreateLabel(vert).SetText('Amount of card pieces with each claimed Cache = ' .. Mod.Settings.cPieces);
-if (Mod.Settings.FixedPieces == false) then
+if Mod.Settings.FixedPieces == false then
 UI.CreateLabel(vert).SetText('Random +/- limit of: ' .. Mod.Settings.Luck);
 end	
 end
