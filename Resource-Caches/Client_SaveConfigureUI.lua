381059d7f74f3e080942f7f75fa8e715a4ee060c
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-08 16:38:28 +0000

    Update Client_SaveConfigureUI.lua

--- Resource-Caches/Client_SaveConfigureUI.lua
+++ Resource-Caches/Client_SaveConfigureUI.lua
@@ -4,5 +4,6 @@ function Client_SaveConfigureUI(alert)
     Mod.Settings.cPieces = numberInputField2.GetValue();
     Mod.Settings.FixedPieces = booleanInputField.GetIsChecked();
     Mod.Settings.Luck = numberInputField3.GetValue();
+    print(Mod.Settings.FixedPieces);
     
 end
