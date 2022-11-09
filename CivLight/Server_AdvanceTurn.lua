122c953ddc257a7b27702a7a7e3d34757012f0b1
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-09 15:51:26 +0000

    Update Server_AdvanceTurn.lua

--- CivLight/Server_AdvanceTurn.lua
+++ CivLight/Server_AdvanceTurn.lua
@@ -5,7 +5,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
       if order.proxyType == "GameOrderAttackTransfer" then 
           if orderResult.IsAttack and orderResult.IsSuccessful then 
           local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To]; 
-			print(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies);
+			print(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies.NumArmies);
                 if attackedTerr.Structures ~= nil then 
                     if attackedTerr.Structures[WL.StructureType.MercenaryCamp] ~= nil then -- there is a mercenary camp on the territory that was successfully attacked -- so now you can do what you want :p
 			Village(game, addNewOrder, order.To, order.PlayerID)
