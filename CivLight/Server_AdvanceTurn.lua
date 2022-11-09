0ba6f9366fec8c2e71f9cfe3d758c5557e54460a
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-09 17:18:25 +0000

    Update Server_AdvanceTurn.lua

--- CivLight/Server_AdvanceTurn.lua
+++ CivLight/Server_AdvanceTurn.lua
@@ -19,10 +19,10 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 				
 				local terrModTo = WL.TerritoryModification.Create(order.To);   
 				terrModTo.SetOwnerOpt = WL.PlayerID.Neutral;
-				terrModTo.AddArmies = round(orderResult.AttackingArmiesKilled.NumArmies);
+				--terrModTo.AddArmies = round(orderResult.AttackingArmiesKilled.NumArmies);
                 
 				local terrModfrom = WL.TerritoryModification.Create(order.From);
-				terrModfrom.SetArmiesTo = terrModfrom.NumArmies.NumArmies;
+				terrModfrom.SetArmiesTo = terrModTo.NumArmies.NumArmies;
 
 				end
 		  end 
