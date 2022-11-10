b14db59c1bb05103b0834fd32b7bc57d9a627c48
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-10 16:41:01 +0000

    Update Client_PresentConfigureUI.lua

--- CivLight/Client_PresentConfigureUI.lua
+++ CivLight/Client_PresentConfigureUI.lua
@@ -5,9 +5,7 @@ function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
   -- initiliase all default values for the inputs
   showMainConfig();
 	
-	local initialValueConvert = Mod.Settings.NumToConvert;
-	local initialValueArmies = Mod.Settings.SetArmiesTo;
-    	local initalcheckbox = Mod.Settings.OnlyBaseNeutrals;
+	
 	
 	local initialVillages = Mod.Settings.NumOfVillages;
 	local ON = Mod.Settings.ONeutrals;
@@ -25,17 +23,7 @@ function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
 
 	local AttackNeutral = Mod.Settings.AttackNeutral;
 	
-	if initialValueConvert == nil then
-		initialValueConvert = 2;
-	end
 	
-	if initialValueArmies == nil then
-		initialValueArmies = 2;
-	end
-
-    if initalcheckbox == nil then 
-        initalcheckbox = false; 
-    end
 	
 	if initialVillages == nil then
 		initialVillages = 3;
@@ -90,21 +78,7 @@ local horz2 = UI.CreateHorizontalLayoutGroup(vert);  --not used but here for ref
 	
 	UI.CreateLabel(vert).SetText('Expansion+ Mod related:');
 	
-	UI.CreateLabel(vert).SetText('amount of neutrals a player shall gain each turn');
-    numberInputField = UI.CreateNumberInputField(vert)
-		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(initialValueConvert);
 	
-	 UI.CreateLabel(vert).SetText('amount of armies a player shall get with the territory');
-    numberInputField2 = UI.CreateNumberInputField(vert)
-		.SetSliderMinValue(1)
-		.SetSliderMaxValue(10)
-		.SetValue(initialValueArmies); 
- 
-        UI.CreateLabel(vert).SetText('only base neutral armies and less shall be claimed');
-        booleanInputField = UI.CreateCheckBox(vert)        
-                .SetIsChecked(initalcheckbox);
 	
 	UI.CreateLabel(vert).SetText('Village Mod related:');
 	
@@ -190,7 +164,39 @@ end
 function showExpansionConfig()
   DestroyWindow();
   SetWindow("FreeExpansion");
-  -- show all settings of the FreeExpansion part
+   
+local initialValueConvert = Mod.Settings.NumToConvert;
+local initialValueArmies = Mod.Settings.SetArmiesTo;
+local initalcheckbox = Mod.Settings.OnlyBaseNeutrals;
+	
+	if initialValueConvert == nil then
+		initialValueConvert = 2;
+	end
+	
+	if initialValueArmies == nil then
+		initialValueArmies = 2;
+	end
+
+        if initalcheckbox == nil then 
+       		initalcheckbox = false; 
+    	end
+	
+	UI.CreateLabel(vert).SetText('amount of neutrals a player shall gain each turn');
+    numberInputField = UI.CreateNumberInputField(vert)
+		.SetSliderMinValue(1)
+		.SetSliderMaxValue(10)
+		.SetValue(initialValueConvert);
+	
+	 UI.CreateLabel(vert).SetText('amount of armies a player shall get with the territory');
+    numberInputField2 = UI.CreateNumberInputField(vert)
+		.SetSliderMinValue(1)
+		.SetSliderMaxValue(10)
+		.SetValue(initialValueArmies); 
+ 
+        UI.CreateLabel(vert).SetText('only base neutral armies and less shall be claimed');
+        booleanInputField = UI.CreateCheckBox(vert)        
+                .SetIsChecked(initalcheckbox);
+	
   CreateButton(GetRoot()).SetText("Return").SetOnClick(showMainConfig);
 end
 
