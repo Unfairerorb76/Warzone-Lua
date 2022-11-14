989886c45b9cf174ef47835e8f54fce9016b1f0e
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-14 15:12:02 +0000

    Delete Client_PresentSettingsUI.lua

--- Client_PresentSettingsUI.lua
+++
@@ -1,9 +0,0 @@
-function Client_PresentSettingsUI(rootParent)
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-
-	UI.CreateLabel(vert).SetText('A Diplomat costs: ' .. Mod.Settings.CostToBuyDiplomat);
-	UI.CreateLabel(vert).SetText('Max priests: ' .. Mod.Settings.MaxDiplomats);
-	UI.CreateLabel(vert).SetText('turns a diplomacy lasts for: ' .. Mod.Settings.turns);,
-
-	UI.CreateLabel(vert).SetText('A Diplomat will always have the same power as 1 army!');
-end
