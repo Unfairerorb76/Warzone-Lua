50c5281008d0c43b6a774f1b8f91fabdafadb0bb
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-14 11:16:24 +0000

    Update Client_SaveConfigureUI.lua

--- CivLight/Client_SaveConfigureUI.lua
+++ CivLight/Client_SaveConfigureUI.lua
@@ -1,8 +1,8 @@
 function Client_SaveConfigureUI(alert)
     
-    Mod.Settings.NumToConvert = numberInputField.GetValue();
-    Mod.Settings.SetArmiesTo = numberInputField2.GetValue();
-    Mod.Settings.OnlyBaseNeutrals = booleanInputField.GetIsChecked();
+    Mod.Settings.NumToConvert = ExpansionInputField.GetValue();
+    Mod.Settings.SetArmiesTo = ExpArmyInputField2.GetValue();
+    Mod.Settings.OnlyBaseNeutrals = ExpBaseInputField.GetIsChecked();
     
      Mod.Settings.NumOfVillages = numberInputField3.GetValue();
     Mod.Settings.Armies = numberInputField4.GetValue();
