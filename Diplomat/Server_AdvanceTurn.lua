f3d985919cbadb6c769305b66ce0377772732707
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-14 17:23:21 +0000

    Update Server_AdvanceTurn.lua

--- Diplomat/Server_AdvanceTurn.lua
+++ Diplomat/Server_AdvanceTurn.lua
@@ -56,7 +56,21 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 		
 		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'Purchased a Diplomat', {}, {terrMod}));
 	end
-    
+    if order.proxyType == "GameOrderAttackTransfer" then
+		if orderResult.IsAttack and hasNoPriest(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies) then
+			if(orderResult.IsSuccessful == false)then
+				local p = order.PlayerID; -- the attacker
+				local p2 = game.ServerGame.LatestTurnStanding.Territories[order.To]OwnerPlayerID; --player that was attacked
+               			if game.Settings.Cards ~= nil then
+					if game.Settings.Cards[WL.CardID.Spy] ~= nil then
+							local instance = WL.NoParameterCardInstance.Create(WL.CardID.Diplomacy);
+							addNewOrder(WL.GameOrderReceiveCard.Create(p, {instance}));
+							addNewOrder(WL.GameOrderPlayCardSpy.Create(instance.ID, p, p, p2));
+					end
+				end
+			end
+		end
+    end
 end
 
 function NumDiplomatsIn(armies)
