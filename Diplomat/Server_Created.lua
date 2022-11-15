ab04c26cd7d1575b8e11c671db3d1e2bd0da9858
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-15 14:00:32 +0000

    Update Server_Created.lua

--- Diplomat/Server_Created.lua
+++ Diplomat/Server_Created.lua
@@ -1,7 +1,7 @@
 function Server_Created(game, settings)
 	local cards = settings.Cards;
 	if settings.Cards[WL.CardID.Diplomacy] == nil then
-		cards[WL.CardID.Diplomacy] = WL.CardGameDiplomacy.Create(1, 0, 0, 0, 1, Mod.Settings.turns);
+		cards[WL.CardID.Diplomacy] = WL.CardGameDiplomacy.Create(1, 0, 0, 0, 1, 2);
 	end
 	for i, v in pairs(game.Settings.Cards) do
 		cards[i] = v;
