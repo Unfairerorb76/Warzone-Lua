6441e1fd4f1be851ead9b9b062b188a480b15e08
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-16 14:18:11 +0000

    Update Server_AdvanceTurn.lua

--- Capitalist/Server_AdvanceTurn.lua
+++ Capitalist/Server_AdvanceTurn.lua
@@ -60,8 +60,9 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 		if orderResult.IsAttack and hasNoCapitalist(game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies) then
 			if deadCapitalist(orderResult.DefendingArmiesKilled) then
 				local p = order.PlayerID; -- the attacker
+				print(p.PlayerIncome.Total);
 				local IncomeAmount = p.PlayerIncome.Total;
-				print(IncomeAmount);
+				
 				IncomeAmount = IncomeAmount * (Mod.Settings.Percentage / 100);
 				addNewOrder(WL.GameOrderEvent.Create(p, "Updated income", {}, {terrMod}, {}, {WL.IncomeMod.Create(p, IncomeAmount, "You have killed a Capitalist and have been sanctioned")}));	
 
