d89ef34998a9a1b011bc0dd9467399949608a0a8
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-11-17 10:07:22 +0000

    Update Client_PresentSettingsUI.lua

--- CivLight/Client_PresentSettingsUI.lua
+++ CivLight/Client_PresentSettingsUI.lua
@@ -2,16 +2,16 @@ function Client_PresentSettingsUI(rootParent, game)
 
   local vert = UI.CreateVerticalLayoutGroup(rootParent);	
   --Expansion+ mod related
-  UI.CreateLabel(vert).SetText('amount of territories(neutrals) you get for free every turn = ' .. Mod.Settings.NumToConvert).SetColor('#07dfe3');
-  UI.CreateLabel(vert).SetText('amount of armies that come with each new territory = ' .. Mod.Settings.SetArmiesTo).SetColor('#07dfe3');
+  UI.CreateLabel(vert).SetText('Amount of territories(neutrals) you get for free every turn = ' .. Mod.Settings.NumToConvert).SetColor('#07dfe3');
+  UI.CreateLabel(vert).SetText('Amount of armies that come with each new territory = ' .. Mod.Settings.SetArmiesTo).SetColor('#07dfe3');
   if (Mod.Settings.initalcheckbox == true) then
-    UI.CreateLabel(vert).SetText('the mod will only claim neutrals that are less than or equal to armies on distributed territories').SetColor('#07dfe3');
+    UI.CreateLabel(vert).SetText('The mod will only claim neutrals that are less than or equal to armies on distributed territories.').SetColor('#07dfe3');
   end
   --village mod related
-  UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages).SetColor('#0780e3');
-  UI.CreateLabel(vert).SetText('amount of armies with each claimed territory = ' .. Mod.Settings.Armies).SetColor('#0780e3');
+  UI.CreateLabel(vert).SetText('Amount of Villages that spawned = ' .. Mod.Settings.NumOfVillages).SetColor('#0780e3');
+  UI.CreateLabel(vert).SetText('Amount of armies with each claimed territory = ' .. Mod.Settings.Armies).SetColor('#0780e3');
   if (Mod.Settings.ONeutrals == false) then
-    UI.CreateLabel(vert).SetText('claimed territories will also take other players').SetColor('#0780e3');
+    UI.CreateLabel(vert).SetText('Claimed territories will also take other players.').SetColor('#0780e3');
   end
   --army caches related
   UI.CreateLabel(vert).SetText('Amount of Army Caches that spawned = ' .. Mod.Settings.NumOfACaches).SetColor('#09cc02');
@@ -29,7 +29,7 @@ function Client_PresentSettingsUI(rootParent, game)
 
   --misc settings
   if Mod.Settings.AttackNeutral == false then
-    UI.CreateLabel(vert).SetText('you are unable to take neutrals by yourself, you can damage them though.').SetColor('#AC0059');
+    UI.CreateLabel(vert).SetText('You are unable to take neutrals by yourself, you can damage them though.').SetColor('#AC0059');
   end	
     
   end
