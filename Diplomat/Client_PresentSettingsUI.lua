c7bcf941571c6fab5c467aaf22f498fbfe939704
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-12-06 18:59:02 +0000

    Update Client_PresentSettingsUI.lua

--- Diplomat/Client_PresentSettingsUI.lua
+++ Diplomat/Client_PresentSettingsUI.lua
@@ -2,8 +2,8 @@ function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
 	UI.CreateLabel(vert).SetText('A Diplomat costs: ' .. Mod.Settings.CostToBuyDiplomat);
-	UI.CreateLabel(vert).SetText('Max priests: ' .. Mod.Settings.MaxDiplomats);
-	UI.CreateLabel(vert).SetText('turns a diplomacy lasts for: ' .. Game.Settings.Cards[WL.CardID.Diplomacy].Duration);
+	UI.CreateLabel(vert).SetText('Max Diplomats: ' .. Mod.Settings.MaxDiplomats);
+	UI.CreateLabel(vert).SetText('When a Diplomat is killed, it will enforce a diplomacy card between both players');
 
 	UI.CreateLabel(vert).SetText('A Diplomat will always have the same power as 1 army!');
 end
