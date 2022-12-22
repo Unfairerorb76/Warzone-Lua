a926544f6a41a2bf861e452ddb65528139acbf74
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-12-22 20:42:43 +0100

    Update Server_AdvanceTurn.lua

--- Meteor Strike/Server_AdvanceTurn.lua
+++ Meteor Strike/Server_AdvanceTurn.lua
@@ -1,87 +1,35 @@
 require('Utilities');
 require('WLUtilities');
+require('RemoveArmies');
 
 function Server_AdvanceTurn_End(game, addNewOrder)
 
-	local terr = {};  --table of neutral territories
-	local terr2 = {}; 
-	local randomNeutralTerr; -- 
-	
-	
-
-    
-	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do	
-		table.insert(terr, terrID);   --gets each territory ID of neutrals
+	if (Mod.Settings.EnableDoomsDay == true) and (game.ServerGame.Game.TurnNumber == Mod.Settings.TurnDoomsDay) then	
+		inflictDamage(game, math.max(getTableLength(game.ServerGame.LatestTurnStanding.Territories) - Mod.Settings.TerrSurvived, 0), 0) -- 0 is a special value that indicates that all armies and special units are removed, and the territory set to neutral
+	else 
+		inflictDamage(game, Mod.Settings.NumOfStrikes, Mod.Settings.ArmiesKilled);
 	end
-
-	for times = 1, Mod.Settings.NumOfStrikes do
-
-		local rand = math.random(#terr);
-		local randomNeutralTerr = terr[rand]; --picks random neutral then gives it too player
-		local terrMod = WL.TerritoryModification.Create(randomNeutralTerr);
-		territory = game.ServerGame.LatestTurnStanding.Territories[randomNeutralTerr]
-
-		terrMod.SetArmiesTo = math.max(0,(territory.NumArmies.NumArmies - Mod.Settings.ArmiesKilled));
-		
-		if (territory.OwnerPlayerID ~= WL.PlayerID.Neutral and terrMod.SetArmiesTo == 0) then 
-			terrMod.SetOwnerOpt = WL.PlayerID.Neutral;
-		end
-		
-		addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Meteor Strike at " .. game.Map.Territories[randomNeutralTerr].Name,{}, {terrMod}), true);
-		
-	end
-
-	
-if (Mod.Settings.EnableDoomsDay == true) then	
-	
-	if (game.ServerGame.Game.TurnNumber == Mod.Settings.TurnDoomsDay) then --doomsday time
-		
-		for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do	
-			table.insert(terr2, terrID);   --gets each territory ID
-                     
-
-		end
-
-		for times = 1, Mod.Settings.TerrSurvived do
-                  
-			local rand = math.random(#terr2);
-			local randomTerr = terr2[rand]; --picks random territory to survive
-
-			local terrMod = WL.TerritoryModification.Create(randomTerr);
-
-			territory = game.ServerGame.LatestTurnStanding.Territories[randomTerr];
-
-			addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Territory Survived " .. game.Map.Territories[randomTerr].Name, {}, {terrMod}), true);
-			table.remove(terr2, rand);
-
-		end
-
-                for i = 1, #terr2 do
-
-		   		
-			local rand = math.random(#terr2);
-			local randomTerr = terr2[rand]; --picks random territory	
-			local terrMod = WL.TerritoryModification.Create(randomTerr);
-			territory = game.ServerGame.LatestTurnStanding.Territories[randomTerr]
-
-
-	
-			terrMod.SetArmiesTo = 0;
-			terrMod.SetOwnerOpt = WL.PlayerID.Neutral;
-			
-			addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Meteorstrike at " .. game.Map.Territories[randomTerr].Name, {}, {terrMod}), true);
-			table.remove(terr2, rand);
-		end
-				
-	end -- apocolypse time	
-		
-		
-		
-end
 		
 end	--last end for the function	
 
- 
+function inflictDamage(game, num, damage)
+	local terr = {};
+	
+	for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do	
+		table.insert(terr, terrID);   --gets each territory ID of neutrals
+	end
+	
+	for times = 1, math.max(num, #terr) do
+		local rand = math.random(#terr);
+		local randomNeutralTerr = terr[rand]; --picks random neutral then gives it too player
+		local terrMod = killArmiesOrTurnNeutral(game, game.ServerGame.LatestTurnStanding.Territories[randomNeutralTerr], damage);
+		local event = WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Meteor Strike at " .. game.Map.Territories[randomNeutralTerr].Name,{}, {terrMod});
+		event.JumpToActionSpotOpt(game.Map.Territories[randomNeutralTerr].MiddlePointX, game.Map.Territories[randomNeutralTerr].MiddlePointY, game.Map.Territories[randomNeutralTerr].MiddlePointX, game.Map.Territories[randomNeutralTerr].MiddlePointY);
+		addNewOrder(event, true);
+		table.remove(terr, randomNeutralTerr);
+	end
+end
+
 
 function getTableLength(t)
 	local a = 0;
