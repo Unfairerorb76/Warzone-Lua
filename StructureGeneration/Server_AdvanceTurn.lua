7e42d71c4b6baf996e84025670ffcdd775ad9991
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-12-23 22:12:26 +0000

    Update Server_AdvanceTurn.lua

--- StructureGeneration/Server_AdvanceTurn.lua
+++ StructureGeneration/Server_AdvanceTurn.lua
@@ -20,8 +20,8 @@ function Server_AdvanceTurn_End(game, addNewOrder, rootParent)
             --CreateMarket(terrMod);
             --showMainConfig(terrMod);
             structures = {};
-          --  structures[WL.StructureType.MercenaryCamp] = -1; 
-           structures[WL.StructureType.Market] = 1;				 terrMod.AddStructuresOpt = structures; addNewOrder(WL.GameOrderEvent.Create({}, 'Placeholder', {}, {terrMod}));
+            structures[WL.StructureType.MercenaryCamp] = -1; 
+          -- structures[WL.StructureType.Market] = 1;				 terrMod.AddStructuresOpt = structures; addNewOrder(WL.GameOrderEvent.Create({}, 'Placeholder', {}, {terrMod}));
             print(1);
             end
         end
