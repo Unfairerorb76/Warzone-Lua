fbb769164d0663775fe359962e2bd9d8d4c94fd6
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-12-24 09:10:36 +0000

    Update Server_AdvanceTurn.lua

--- StructureGeneration/Server_AdvanceTurn.lua
+++ StructureGeneration/Server_AdvanceTurn.lua
@@ -17,7 +17,7 @@ function Server_AdvanceTurn_End(game, addNewOrder, rootParent)
         if terrSelected.Structures ~= nil then
             if terrSelected.Structures[WL.StructureType.MercenaryCamp] ~= nil then --finds each territory ID of territories with a merc camp
             
-            CreateMarket(terrID, terrSelected);
+            CreateMarket(terrID, terrSelected, addNewOrder);
             --showMainConfig(terr);
             
             print(1);
