04616f61e15503d831977dc50a8a8c3b6e47a280
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-12-25 20:52:56 +0000

    Update Server_AdvanceTurn.lua

--- StructureGeneration/Server_AdvanceTurn.lua
+++ StructureGeneration/Server_AdvanceTurn.lua
@@ -15,8 +15,9 @@ if (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'GetCapit
 --print(tonumber(string.sub(order.Payload, 10)));
 --print(tonumber(order.Payload))
   local terrID = tonumber(string.sub(order.Payload, 13));
- -- print(terrID);
+  print(terrID);
   if terrID ~= nil then
+print(11);
   local terrSelected = game.ServerGame.LatestTurnStanding.Territories[terrID];
   SpecialUnit(terrID, addNewOrder, order, game, terrSelected); 
 end end
