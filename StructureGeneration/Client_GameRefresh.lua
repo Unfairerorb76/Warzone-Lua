d2428b76f420101bfdba9dab23bbde06eac6a32e
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-12-26 07:33:28 +0000

    Update Client_GameRefresh.lua

--- StructureGeneration/Client_GameRefresh.lua
+++ StructureGeneration/Client_GameRefresh.lua
@@ -3,14 +3,18 @@ Alerted = false;
 function Client_GameRefresh(game)
     
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
-		UI.Alert("You must update your app to the latest version to use the Tanks mod");
+		UI.Alert("You must update your app to the latest version to use the Special Units Structures modpack");
         Alerted = true;
 	end
-
 	if(game.Us == nil)then
 		return;
 	end
 
-	
+local data = Mod.PublicGameData;
+
+  UI.Alert(data.Counters[game.Us]);
+
+Mod.PublicGameData = data;
+
 
 end
