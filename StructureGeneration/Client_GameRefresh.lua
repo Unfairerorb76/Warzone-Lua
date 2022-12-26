a81a0a95db427cab509c2fcd0a49f3aca4fac7c3
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2022-12-26 09:49:49 +0000

    Update Client_GameRefresh.lua

--- StructureGeneration/Client_GameRefresh.lua
+++ StructureGeneration/Client_GameRefresh.lua
@@ -11,18 +11,18 @@ function Client_GameRefresh(game)
 	end
 
 for p, _ in pairs(game.Game.PlayingPlayers) do
- if p == game.Us then
+ if p == game.Us.ID then
    print(123);
  end
 end
 
 local data = Mod.PublicGameData;
 print(data);
-if data.Counters[game.Us] == nil then
-data.Counters[game.Us] = 0;
+if data.Counters[game.Us.ID] == nil then
+data.Counters[game.Us.ID] = 0;
 print(data.Counters);
 end
-    UI.Alert(data.Counters[game.Us]);
-    UI.Alert("placeholder" .. data.Counters[game.Us]);
+    UI.Alert(data.Counters[game.Us.ID]);
+    UI.Alert("placeholder" .. data.Counters[game.Us.ID]);
 
 end
