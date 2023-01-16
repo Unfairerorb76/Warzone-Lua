4723f0a97904cebc13746cf84b9de872d1dc1de6
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-16 11:02:43 +0000

    Update Server_GameCustomMessage.lua

--- StructureGeneration/Server_GameCustomMessage.lua
+++ StructureGeneration/Server_GameCustomMessage.lua
@@ -1,33 +1,29 @@
 function Server_GameCustomMessage(game, playerId, payload, setReturn)
 local data = Mod.PublicGameData;
---Need to get the payload type to find out what action to take
- 
-  if payload.Type == "UpdatingCounter" then
-    Count = payload.numStruct;
-    if not payload then
-      setReturn(data.Counters[playerId]);
+  --We need this if statement to ensure that the mod does not break if payload == nil
+  if not payload then
       return;
     else
-      data.Counters[playerId] = Count - 1;
-    end
-  Mod.PublicGameData = data;
-  SetReturn(data.Counters[playerId]);  
+  
+  --Need to get the payload type to find out what action to take
+  if payload.Type == "UpdatingCounter" then
+    --Init variables
+    Count = payload.numStruct;
+   --take away one from players total
+    data.Counters[playerId] = Count - 1;
+    --input data to server
+    Mod.PublicGameData = data;
+    SetReturn(data.Counters[playerId]);  
   end
 
   if payload.Type == "UpdatingMarkets" then
-   --Init variables
-    numMarket = payload.numStruct;
-   -- We need this if statement to ensure that the mod does not break if payload == nil
-    if not payload then
-      setReturn(data.Markets[playerId]);
-      return;
-    else
+    --Init variables
+    numMarket = payload.numStruct;   
     --Add one to players Market total 
-      data.Markets[playerId] = numMarket + 1;
-    end
+    data.Markets[playerId] = numMarket + 1;
     --need to input the data to server and end function
-  Mod.PublicGameData = data;
-  setReturn(data.Markets[playerId]);
+    Mod.PublicGameData = data;
+    setReturn(data.Markets[playerId]);
   end
   --Next type to go below with an elseif instead of the end above
 end
