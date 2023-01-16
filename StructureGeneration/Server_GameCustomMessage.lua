30ac6cc1f6ca1cf3468d9af21e9f837cf0742a38
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-16 12:22:07 +0000

    Update Server_GameCustomMessage.lua

--- StructureGeneration/Server_GameCustomMessage.lua
+++ StructureGeneration/Server_GameCustomMessage.lua
@@ -1,22 +1,8 @@
 function Server_GameCustomMessage(game, playerId, payload, setReturn)
-local data = Mod.PublicGameData;
+data = Mod.PublicGameData;
   --We need this if statement to ensure that the mod does not break if payload == nil
   if not payload then
       return;
-  end
-  
-  --Need to get the payload type to find out what action to take
-  if payload.type == "UpdatingCounter" then
-    --Init variables
-    count = payload.numStruct;
-    print(count);
-   --take away one from players total
-    print(data.Counters[playerId]);
-    data.Counters[playerId] = count - 1;
-    print(data.Counters[playerId]);
-    --input data to server
-    Mod.PublicGameData = data;
-    return(data.Counters[playerId]);  
   end
 
   if payload.type == "UpdatingMarkets" then
@@ -24,9 +10,16 @@ local data = Mod.PublicGameData;
     numMarket = payload.numStruct;   
     --Add one to players Market total 
     data.Markets[playerId] = numMarket + 1;
+    --subtract one from players village count
+    data.Counters[playerId] = PlayerCount(data.Counters[playerId]);
     --need to input the data to server and end function
     Mod.PublicGameData = data;
     return(data.Markets[playerId]);
   end
   --Next type to go below with an elseif instead of the end above
+end
+
+function PlayerCount(count)
+  count = count - 1;
+  return(count);
 end
