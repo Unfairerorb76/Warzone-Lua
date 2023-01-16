97e146c20429e3da0872fd545829a890f1653e1a
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-16 10:49:34 +0000

    Update Server_GameCustomMessage.lua

--- StructureGeneration/Server_GameCustomMessage.lua
+++ StructureGeneration/Server_GameCustomMessage.lua
@@ -1,12 +1,17 @@
 function Server_GameCustomMessage(game, playerId, payload, setReturn)
-	local data = Mod.PublicGameData;
- 	if not payload then
-	   setReturn(data.Markets);
-	   return;
-	else
-    
-        data.Markets[playerId] = payload.Markets;
-	end
-	Mod.PublicGameData = data;
-	setReturn(data.Markets);
+local data = Mod.PublicGameData;
+--Need to get the payload type to find out what action to take
+  if payload.type == "UpdatingMarkets" then
+   -- We need this if statement to ensure that the mod does not break if payload == nil
+    if not payload then
+      setReturn(data.Markets[playerId]);
+      return;
+    else
+    --Add one to players Market total 
+      data.Markets[playerId] = payload + 1;
+    end
+    --need to input the data to server and end function
+  Mod.PublicGameData = data;
+  setReturn(data.Markets);
+  end
 end
