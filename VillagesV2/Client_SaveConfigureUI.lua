a2c094aa3cca0f71ce295f306151ce5c60f9c356
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-17 13:46:37 +0000

    Update Client_SaveConfigureUI.lua

--- VillagesV2/Client_SaveConfigureUI.lua
+++ VillagesV2/Client_SaveConfigureUI.lua
@@ -1,5 +1,22 @@
 function Client_SaveConfigureUI(alert)
     
     Mod.Settings.NumOfVillages = numberInputField.GetValue();
+    Mod.Settings.Armies = numberInputField2.GetValue();
+    Mod.Settings.ONeutrals = booleanInputField.GetIsChecked();
     
+    local maxCap = numberInputField3.GetValue();
+    if maxCap < 1 or maxCap > 5 then alert("Max number of Capitalists per player must be between 1 and 5"); end
+    Mod.Settings.CapitalistLimit = maxCap;
+    
+    local maxDip = numberInputField4.GetValue();
+    if maxDip < 1 or maxDip > 5 then alert("Max number of Diplomats per player must be between 1 and 5"); end
+    Mod.Settings.DiplomatLimit = maxDip;
+    
+    local maxMed = numberInputField5.GetValue();
+    if maxMed < 1 or maxMed > 5 then alert("Max number of Medics per player must be between 1 and 5"); end
+    Mod.Settings.MedicLimit = maxMed;
+    
+    local maxPri = numberInputField6.GetValue();
+    if maxPri < 1 or maxPri > 5 then alert("Max number of Priests per player must be between 1 and 5"); end
+    Mod.Settings.PriestLimit = maxPri;
 end
