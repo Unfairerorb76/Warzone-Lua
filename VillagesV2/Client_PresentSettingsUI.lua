c694bf32cf187862d243da3bac0cb4f1e0c2a3a9
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-17 14:03:58 +0000

    Update Client_PresentSettingsUI.lua

--- VillagesV2/Client_PresentSettingsUI.lua
+++ VillagesV2/Client_PresentSettingsUI.lua
@@ -1,13 +1,13 @@
 function Client_PresentSettingsUI(rootParent)
 	
     local vert = UI.CreateVerticalLayoutGroup(rootParent);	
-    UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages).SetColor('#0000FF');   
-    UI.CreateLabel(vert).SetText('amount of armies with each claimed territory = ' .. Mod.Settings.Armies).SetColor('#0000FF');
+    UI.CreateLabel(vert).SetText('amount of villages that spawned = ' .. Mod.Settings.NumOfVillages).SetColor('#4EFFFF');   
+    UI.CreateLabel(vert).SetText('amount of armies with each claimed territory = ' .. Mod.Settings.Armies).SetColor('#4EFFFF');
 
 if (Mod.Settings.ONeutrals == true) then
-   UI.CreateLabel(vert).SetText('Villages will only take neutral territories that border it.').SetColor('#0000FF');
+   UI.CreateLabel(vert).SetText('Villages will only take neutral territories that border it.').SetColor('#4EFFFF');
 else
-   UI.CreateLabel(vert).SetText('Villages will take every territory that borders it.').SetColor('#0000FF');
+   UI.CreateLabel(vert).SetText('Villages will take every territory that borders it.').SetColor('#4EFFFF');
 end
 --Special Unit Limits	
    UI.CreateLabel(vert).SetText('Limit on Capitalists = ' .. Mod.Settings.CapitalistLimit).SetColor('#AD7E7E');
