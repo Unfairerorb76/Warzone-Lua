b85f24296d578286c625f5150b25e6974a1a6d2b
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-18 06:48:56 +0000

    Update Server_AdvanceTurn.lua

--- TerritoryLimit/Server_AdvanceTurn.lua
+++ TerritoryLimit/Server_AdvanceTurn.lua
@@ -3,8 +3,11 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 
 	list = {};
 	count = {};
+	local pTable = {};
+
 	for p, _ in pairs(game.Game.PlayingPlayers) do
 		count[p] = {};
+		pTable[p] = {};
 	end
 
 	for playerID, _ in pairs(game.Game.PlayingPlayers) do
@@ -13,10 +16,38 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 			table.insert(count[game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID], terrID);
 		end
 	end
-	print(getTableLength(count[playerID]));
-	print(count);
+	for p, arr in pairs(count[playerID]) do
+		if getTableLength(count[playerID]) > 10 then
+			local min = 9999;
+			for p, arr in pairs(count[playerID]) do
+			if game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies < min then
+				local PlayerTerr = arr;
+				min = PlayerTerr.NumArmies;
+			end
+			end
+			if PlayerTerr == nill then break; end
+			local terrMod = WL.TerritoryModification.Create(PlayerTerr);
+			terrMod.SetOwnerOpt = WL.PlayerID.Neutral;
+			table.insert(list, terrMod);
+			table.remove(arr, rand);
+		end
+	end
+	table.insert(pTable[p], WL.GameOrderEvent.Create(p,"new territory",{}, list));
 	end
 	
+	local i = 1;
+	local addedOrders = true;
+	while addedOrders do
+ 	 addedOrders = false;
+ 	 for p, _  in pairs(game.Game.PlayingPlayers) do
+    	   if pTable[p][i] ~= nil then
+     	   addedOrders = true;
+           addNewOrder(pTable[p][i]);
+    	   end
+	 end
+         i = i + 1;
+	end
+
 end
 
 function getTableLength(t)
