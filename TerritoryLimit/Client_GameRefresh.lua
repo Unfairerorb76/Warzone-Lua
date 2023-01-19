89873ab2c71331bc9e94017d5f78cd838793e4dd
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-19 13:34:03 +0000

    Update Client_GameRefresh.lua

--- TerritoryLimit/Client_GameRefresh.lua
+++ TerritoryLimit/Client_GameRefresh.lua
@@ -2,13 +2,10 @@ Alerted = false;
 
 function Client_GameRefresh(game)
   	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.22")) then
-		UI.Alert("You must update your app to the latest version to use the Special Units Structures modpack");
+		UI.Alert("You must update your app to the latest version to use this mod!");
         Alerted = true;
 	end
 	if(game.Us == nil)then
 		return;
-	end
-        if game.Game.TurnNumber < 1 or data.Counters[game.Us.ID] == 0 then		
-         return;	
-        end	 
+	end	 
 end
