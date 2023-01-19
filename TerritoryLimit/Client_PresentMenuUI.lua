d2a9a682fdd926681c6d39d538f8e31821a0ec03
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-19 06:45:13 +0000

    Update Client_PresentMenuUI.lua

--- TerritoryLimit/Client_PresentMenuUI.lua
+++ TerritoryLimit/Client_PresentMenuUI.lua
@@ -5,7 +5,8 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close
   data = Mod.PublicGameData;
    Init(rootParent);  
    local vert = CreateVert(GetRoot());
-   local limit =  data.TerrLimit[game.Us.ID];
+   local limit = {};
+  limit = data.TerrLimit[game.Us.ID];
   
     if game.Us == nil then
        return; 
