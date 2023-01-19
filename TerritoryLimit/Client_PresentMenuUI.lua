6c3c97720f904b2206585a34ba198d6f38cb89d7
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-19 06:38:27 +0000

    Update Client_PresentMenuUI.lua

--- TerritoryLimit/Client_PresentMenuUI.lua
+++ TerritoryLimit/Client_PresentMenuUI.lua
@@ -6,5 +6,9 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close
    Init(rootParent);  
    local vert = CreateVert(GetRoot());
   
-   CreateLabel(vert).SetText('You currently hold ' .. data.TerrLimit .. 'territories').SetColor('#606060');
+    if game.Us == nil then
+       return; 
+    end
+  
+   CreateLabel(vert).SetText('You currently hold ' .. data.TerrLimit[Game.Us.ID] .. 'territories').SetColor('#FFAF56');
 end
