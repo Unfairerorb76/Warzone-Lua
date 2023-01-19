32e37b949766bfe73825663639d51c8ca14ec825
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-19 13:35:40 +0000

    Create Client_GameRefresh.lua

---
+++ Warzone2069/Client_GameRefresh.lua
@@ -0,0 +1,18 @@
+require('LoreBook');
+Alerted = false;
+
+function Client_GameRefresh(game)
+ local data = Mod.PublicGameData;   
+	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
+		UI.Alert("You must update your app to the latest version to use the Special Units Structures modpack");
+        Alerted = true;
+	end
+	if(game.Us == nil)then
+		return;
+	end
+        if game.Game.TurnNumber < 1 or data.Counters[game.Us.ID] == 0 then		
+         return;	
+        end	 
+  
+  UI.Alert(advert());
+end
