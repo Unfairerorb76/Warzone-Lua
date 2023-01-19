6278b0e6ebb232554ece242f1a148de7d4286e79
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-19 06:34:08 +0000

    Update Client_PresentConfigureUI.lua

--- TerritoryLimit/Client_PresentConfigureUI.lua
+++ TerritoryLimit/Client_PresentConfigureUI.lua
@@ -9,7 +9,7 @@ function Client_PresentConfigureUI(rootParent, rootParent2, rootParent3)
         local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
     local row1 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row1).SetText('Limit the amount of territories that a player can own');
+	UI.CreateLabel(row1).SetText('Limit the amount of territories that a player can own').SetColor('#AD7E7E');
     limitInputField = UI.CreateNumberInputField(row1)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(50)
