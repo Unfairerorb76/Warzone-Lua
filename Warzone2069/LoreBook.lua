bab1728c788eceba41073cf90fa632d8a1ca123a
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-01-19 14:30:55 +0000

    Update LoreBook.lua

--- Warzone2069/LoreBook.lua
+++ Warzone2069/LoreBook.lua
@@ -1,9 +1,52 @@
 function advert()
-	--List given by Fizzer of all supported colors. 74 diffrent colors
 	local adverts = {
-		"hello",
-    "world"
-	};
+	"Breaking news: Rick Ashley’s body was stolen by cultists from the cemetery today, body snatchers post video stating that they “are never gonna give him up”",
+    	"Police provide public statement on latest abduction of Rick Ashley’s body: “we promise to never let you down in our hunt for the thief”",
+	"Docu-series: the tragedy of clan league 42 has released today with critics saying it’s an instant horror classic",
+	"Tired of your boring life, where everything is always the same old crap? Start a new life!	UFO Abduction Services® makes leaving your past life behind an outer world experience! Contact us at UFO-76-UFO-76 for a free trial probing!",
+	"Help Wanted: Participant in cut-throat competition involving deceit, betrayal, and a BOX!",
+	"HangFire's Amusement Park - Featuring Day and Night Demolition Shows. Open Tuesday thru Sunday 9 AM to Midnight. First time visitors and children under 12 receive one free stick of dynamite and 50% off Entry fee.",
+	"HangFire's Amusement Park - Activities include: M-60 Firing Range, Grenade Pit, Mortar and Bazooka Range, Close Quarters Combat, Landmine Racing and more!",
+	"HangFire's Amusement Park - Fun for the whole family. Come one, come all. Reservations and Family Packages Available. Call 1-444-EXPLODE (397-5633), Sponsored by Seefore Productions and Blow My Mind Supply Store!",
+	"Nigerian prince needs help getting inheritance back. Mail me for more info: musaibrahim@Warzone.com",
+	"New chest enlargement pills!!! More info at myplacebo.Warzone.com",
+	"Why Warzone was almost a fizzer. Today, at 20:00, only at warlight news.",
+	"Breaking news! Activision suit finally finished. Call of Duty: Warlight renames back to Call of Duty: Warzone.",
+	"Lionheart - who was this mapmaking furry? This and more at ZoneWar weekly.",
+	"Grover",
+	"Zonop tries to revive CL after the Let's Fight fiasco. Will she succeed? This and more at ZoneWar weekly.",
+	"Discombobulator's blockade - did Timinator use it first? This and more at ZoneWar weekly.",
+	"Letting everybody know, that Rachel (+1-202-555-0121) is a bloody harlot! I am divorcing you scoundrel!1!1!1!!",
+	"Fancy dot's appear all over western hemisphere, scientists are baffled!",
+	"Warzone: 2069, created and sponsored by a flying UFO!",
+	"Latest study shows that 86% of office hours are spent playing Warzone instead of working.",
+	"new calendar explodes into popularity, years are now marked as BW (Before Warzone) and AW (After Warzone), 0AW set to be what was once 2020",
+	"Breaking news: a new study finds that 95% of people will believe 'facts' if you claim it is a new study.",
+	"A man was admitted to the hospital with 25 toy horses shoved up his rectum. Doctors described his condition as stable.",
+	"Doctor was too busy with CW to even visit the patient. Diagnosis considered questionable.",
+	"TR-8R EXPOSES COMPANY WIDE CORRUPTION, Details on ZoneWar News at 6.",
+	"Interview with Fizzer - the creator of Warzone and the first person to be uploaded to AI. This and more at ZoneWar weekly.",
+	"New update - minus one army must stand guard. What does it mean to the community? This and more at ZoneWar weekly.",
+	"Youtube bans Warzone streaming. Streamers move to Cornhub. This and more at ZoneWar weekly.",
+	"Breaking news! Riots at Finnish-Chinese borders.",
+	"War on Ukraine after 47 years finally resolved? Country leaders agree to play 'guiroma' for contested areas. This and more at ZoneWar weekly.",
+	"World Health Organisation finds torture by Siege LD as cruel and inhuman. This and more at ZoneWar weekly.",
+	"MASTERS clan - from elite to 'newbie boot camp'. Unique interview with the clan leadership. This and more at ZoneWar weekly.",
+	"Warzone and derivative - part IV of Warzone and math. This and more at ZoneWar weekly.",
+	"Want to win this game? Give 1000 coins to JK_3 and get nothing! You dont think it an amazing deal? That's why you're losing my friend..",
+	"Do you like this mod, check out my other creations such as CivLight and Meteor strike.",
+	"North Korean basin declared nuclear free zone after 40 years.",
+	"Scotland tourism soars after the Edinburgh - Glasgow Canal finally opens",
+	"New element was found today, Scientists are calling it 'fizzium'",
+	"UN declares that all future conflicts must take place on Warzone after the deadly 1 day war kills 5% of world population",
+	"UN declares that doplhins are as intelligent as people, ambassador for the species to be met today",
+	"JK_3, the alt master, overruns server with 1000's of accounts. Find out more at ZoneWar weekly!"
+	"Just_A_Dutchman completes 1000th mod, wins noble prize. Find out more at ZoneWar weekly",
+	"Grover's Cafe launchs first cafe on the moon, 'It was actually quite easy, barely an inconvenience' - Grover",
+    	"Fizzer becomes first trillionaire. Find out more at ZoneWar weekly",
+	"Join the Warzone Modding discord at: https://discord.gg/2FzGWFPv",
+	"Error: Idiot has joined the game"
+};
 	local rand = adverts[math.random(#adverts)]
 	return rand
 end
