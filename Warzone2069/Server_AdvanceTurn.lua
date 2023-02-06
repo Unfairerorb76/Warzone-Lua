e2775d3260c7ef43b419ea643f5869d5f0b0a749
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-02-06 13:17:55 +0000

    Update Server_AdvanceTurn.lua

--- Warzone2069/Server_AdvanceTurn.lua
+++ Warzone2069/Server_AdvanceTurn.lua
@@ -2,10 +2,24 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 
    if order.proxyType == "GameOrderAttackTransfer" then
     if orderResult.IsSuccessful then
-    local r = math.random(1,25);
+    local r = math.random(1,2);
+    local list = {};
     print(r);
-	if (r == 25) then
-          
+	if (r == 2) then
+		local attackedTerr = game.ServerGame.LatestTurnStanding.Territories[order.To];
+		local attackerTerr = game.ServerGame.LatestTurnStanding.Territories[order.From];		
+          	local terrModTo = WL.TerritoryModification.Create(order.To); 		
+		terrModTo.SetOwnerOpt = attackedTerr.OwnerPlayerID;
+		terrModTo.SetArmiesTo = (attackedTerr.NumArmies.NumArmies);
+              
+		local terrModfrom = WL.TerritoryModification.Create(order.From);
+		terrModfrom.SetArmiesTo = (attackerTerr.NumArmies.NumArmies);
+				
+		--addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Rubberbanding", {}, {terrModTo}));
+		--addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "returning damaged troops",{}, {terrModfrom}), true);
+		table.insert(list, terrModfrom);
+		table.insert(list, terrModTo);
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Rubberbanding",{}, list), true);
      	end
    end
   
