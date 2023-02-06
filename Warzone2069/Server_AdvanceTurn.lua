0ada47154dd29a41044017f32b0dc8d5ba0212b7
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-02-06 12:38:19 +0000

    Create Server_AdvanceTurn.lua

---
+++ Warzone2069/Server_AdvanceTurn.lua
@@ -0,0 +1,10 @@
+function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
+
+   if order.proxyType == "GameOrderAttackTransfer" then
+    local r = math.random(1,20)
+		 if (r = 20) then
+			print(2);
+     end
+   end
+  
+end
