260a2eed0d34d4bb3f452dc7de5fc6812f84591a
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-02-07 19:03:38 +0000

    Update Client_GameRefresh.lua

--- Warzone2069/Client_GameRefresh.lua
+++ Warzone2069/Client_GameRefresh.lua
@@ -3,7 +3,7 @@ Alerted = false;
 
 function Client_GameRefresh(game)
  local data = Mod.PublicGameData; 
- print(data.Gullible[game.Us.Id]);
+ print(data.Gullible[game.Us.ID]);
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
 		UI.Alert("You must update your app to the latest version to use the Special Units Structures modpack");
         Alerted = true;
