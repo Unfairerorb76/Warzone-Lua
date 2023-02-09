81ff34dc82f57900b4185588eaf9284ac2a4725f
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-02-09 14:30:59 +0000

    Update Server_GameCustomMessage.lua

--- Warzone2069/Server_GameCustomMessage.lua
+++ Warzone2069/Server_GameCustomMessage.lua
@@ -7,7 +7,8 @@ data = Mod.PublicGameData;
   if payload.type == "UpdatingGullible" then
     --Init variables
     data.Gullible[playerId] = True;
-    print(13);
+    print(playerId]);
+    print(data.Gullible[playerId]);
     --need to input the data to server and end function
     Mod.PublicGameData = data;
     return(data.Gullible[playerId]);
