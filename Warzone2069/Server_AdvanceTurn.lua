58c39cc64f4adaa924167e47e6f5f32a740b9996
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-02-09 15:40:22 +0000

    Update Server_AdvanceTurn.lua

--- Warzone2069/Server_AdvanceTurn.lua
+++ Warzone2069/Server_AdvanceTurn.lua
@@ -28,7 +28,7 @@ local t = {};
 for p, _ in pairs(game.Game.PlayingPlayers) do
 	local IncomeAmount = 1;
 	addNewOrder(WL.GameOrderEvent.Create(p, "Paying Taxes", {}, {}, {}, {WL.IncomeMod.Create(p, -IncomeAmount, "Paying taxes")}));
-	if data.Viewed[p] == true;
+	if data.Viewed[p] == true then
 	table.insert(t, data.Viewed[p]);
 	end
 	if count == #t then
