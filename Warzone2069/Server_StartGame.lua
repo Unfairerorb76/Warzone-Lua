ce24eccf04e80b665bb447045fc2a0ee5e0f0dd1
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-02-09 15:30:56 +0000

    Update Server_StartGame.lua

--- Warzone2069/Server_StartGame.lua
+++ Warzone2069/Server_StartGame.lua
@@ -3,8 +3,10 @@ require('UI');
 function Server_StartGame(game, standing)
   local data = Mod.PublicGameData;
   data.Gullible = {};
+  data.Viewed = {};
   for p, _ in pairs(game.Game.PlayingPlayers) do
     data.Gullible[p] = false;
+    data.Viewed[p] = true;
   end
   Mod.PublicGameData = data;
 end
