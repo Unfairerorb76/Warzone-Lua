6263642d556a64a925dc0a135a095b435332aaa9
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-03-28 09:43:54 +0100

    Update Client_PresentMenuUI.lua

--- Warzone2069/Client_PresentMenuUI.lua
+++ Warzone2069/Client_PresentMenuUI.lua
@@ -27,6 +27,7 @@ function showMenu()
     CreateLabel(vert).SetText('This is the main menu screen, select the buttons below related on what you want to do.').SetColor('#606060');
     CreateButton(horz).SetText("Shop").SetOnClick(shopMenu).SetColor('#00FF8C');
     CreateButton(horz).SetText("Battlepass").SetOnClick(BPMenu);
+    CreateButton(horz).SetText("Credits").SetOnClick(showCredits);
     if (r == 50) then
     CreateButton(horz).SetText("Admin Controls").SetOnClick(adminMenu).SetColor('#ff0000');
     end
