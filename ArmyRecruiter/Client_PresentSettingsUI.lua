d9f0fac6d5a2b4d45f5354ddde21d52f2bc3fb6e
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-04-27 20:30:01 +0100

    Update Client_PresentSettingsUI.lua

--- ArmyRecruiter/Client_PresentSettingsUI.lua
+++ ArmyRecruiter/Client_PresentSettingsUI.lua
@@ -3,8 +3,8 @@ function Client_PresentSettingsUI(rootParent)
 
 	UI.CreateLabel(vert).SetText('A Recruiter generates armies at the end of each turn on the territory it occupies');
 
-	UI.CreateLabel(vert).SetText('A Recruiter costs: ' .. Mod.Settings.CostToBuyRecruiter .. ' gold.').SetColor('#5d535d');
-	UI.CreateLabel(vert).SetText('Max Recruiter: ' .. Mod.Settings.MaxRecruiter).SetColor('#5d535d');
-	UI.CreateLabel(vert).SetText('Number of armies generated per turn per Recruiter: '.. Mod.Settings.NumArmies).SetColor('#5d535d');
-	UI.CreateLabel(vert).SetText('A Recruiter will always have the same power as 3 armies!').SetColor('#5d535d');
+	UI.CreateLabel(vert).SetText('A Recruiter costs: ' .. Mod.Settings.CostToBuyRecruiter .. ' gold.').SetColor('#800080');
+	UI.CreateLabel(vert).SetText('Max Recruiter: ' .. Mod.Settings.MaxRecruiters).SetColor('#800080');
+	UI.CreateLabel(vert).SetText('Number of armies generated per turn per Recruiter: '.. Mod.Settings.NumArmies).SetColor('#800080');
+	UI.CreateLabel(vert).SetText('A Recruiter will always have the same power as 3 armies!').SetColor('#800080');
 end
