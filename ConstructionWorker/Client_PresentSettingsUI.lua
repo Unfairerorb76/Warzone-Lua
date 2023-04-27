13429fc0d8265724fc9a2cd8aacaa985b10a1368
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-04-27 20:46:34 +0100

    Create Client_PresentSettingsUI.lua

---
+++ ConstructionWorker/Client_PresentSettingsUI.lua
@@ -0,0 +1,10 @@
+function Client_PresentSettingsUI(rootParent)
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+
+	UI.CreateLabel(vert).SetText('A Recruiter generates armies at the end of each turn on the territory it occupies.');
+
+	UI.CreateLabel(vert).SetText('A Worker costs: ' .. Mod.Settings.CostToBuyWorker .. ' gold.').SetColor('#800080');
+	UI.CreateLabel(vert).SetText('Max Workers: ' .. Mod.Settings.MaxWorkers).SetColor('#800080');
+	UI.CreateLabel(vert).SetText('Number of Cities generated per turn per Worker: '.. Mod.Settings.NumCities).SetColor('#800080');
+	UI.CreateLabel(vert).SetText('A Worker will always have the same power as 3 armies!').SetColor('#800080');
+end
