03c23470342559d51b443f4ff8693f7d24708eb5
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-04-27 20:30:51 +0100

    Update Client_PresentSettingsUI.lua

--- ArmyRecruiter/Client_PresentSettingsUI.lua
+++ ArmyRecruiter/Client_PresentSettingsUI.lua
@@ -1,7 +1,7 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
-	UI.CreateLabel(vert).SetText('A Recruiter generates armies at the end of each turn on the territory it occupies');
+	UI.CreateLabel(vert).SetText('A Recruiter generates armies at the end of each turn on the territory it occupies.');
 
 	UI.CreateLabel(vert).SetText('A Recruiter costs: ' .. Mod.Settings.CostToBuyRecruiter .. ' gold.').SetColor('#800080');
 	UI.CreateLabel(vert).SetText('Max Recruiter: ' .. Mod.Settings.MaxRecruiters).SetColor('#800080');
