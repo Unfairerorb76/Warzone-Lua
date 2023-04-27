9f0cc8ef1129c5b6e7140a4f8b95025dfec67d13
Unfairerorb76 <99406899+Unfairerorb76@users.noreply.github.com>
2023-04-27 18:48:36 +0100

    Update Server_AdvanceTurn.lua

--- ArmyRecruiter/Server_AdvanceTurn.lua
+++ ArmyRecruiter/Server_AdvanceTurn.lua
@@ -63,11 +63,11 @@ for terrID, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) d
 	if (terr.IsNeutral == false) then
 			
  		if hasNoRecruiter(terr.NumArmies) then
-			for (times = 1, NumRecruitersIn(terr.NumArmies)) do
+			--for (times = 1, NumRecruitersIn(terr.NumArmies)) do
 				local terrMod = WL.TerritoryModification.Create(terr);
 				terrMod.SetArmiesTo = terr.NumArmies + Mod.Settings.NumArmies;
 				addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID, "New armies recruited", {}, terrMod)):	
-			end
+			--end
 		end
 	end
 end 
