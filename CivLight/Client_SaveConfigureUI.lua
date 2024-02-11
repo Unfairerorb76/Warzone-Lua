16c3d90f3163005f80c0aed9f1b564adea208119
Jack Millar <99406899+Unfairerorb76@users.noreply.github.com>
2024-02-11 21:15:18 +0000

    Update Client_SaveConfigureUI.lua

--- CivLight/Client_SaveConfigureUI.lua
+++ CivLight/Client_SaveConfigureUI.lua
@@ -4,54 +4,52 @@ function Client_SaveConfigureUI(alert)
                 alert("You must open and close every button in CivLight once before you can save settings (sorry).");
         end
         if ExpansionInputField == nil then
-         Mod.Settings.NumToConvert = 2;
-         Mod.Settings.SetArmiesTo = 2;
-         Mod.Settings.OnlyBaseNeutrals = false;
+                Mod.Settings.NumToConvert = 2;
+                Mod.Settings.SetArmiesTo = 2;
+                Mod.Settings.OnlyBaseNeutrals = false;
         else
-         Mod.Settings.NumToConvert = ExpansionInputField.GetValue();
-         Mod.Settings.SetArmiesTo = ExpArmyInputField.GetValue();
-         Mod.Settings.OnlyBaseNeutrals = ExpBaseInputField.GetIsChecked();
+                Mod.Settings.NumToConvert = ExpansionInputField.GetValue();
+                Mod.Settings.SetArmiesTo = ExpArmyInputField.GetValue();
+                Mod.Settings.OnlyBaseNeutrals = ExpBaseInputField.GetIsChecked();
         end
         
-       if villageInputField == nil then
-        Mod.Settings.NumOfVillages = 3;
-        Mod.Settings.Armies = 2;
-        Mod.Settings.ONeutrals = true;   
-      else
-        Mod.Settings.NumOfVillages = villageInputField.GetValue();
-        Mod.Settings.Armies = vValueInputField.GetValue();
-        Mod.Settings.ONeutrals = ONInputField.GetIsChecked();
-      end
+        if villageInputField == nil then
+                Mod.Settings.NumOfVillages = 3;
+                Mod.Settings.Armies = 2;
+                Mod.Settings.ONeutrals = true;   
+        else
+                Mod.Settings.NumOfVillages = villageInputField.GetValue();
+                Mod.Settings.Armies = vValueInputField.GetValue();
+                Mod.Settings.ONeutrals = ONInputField.GetIsChecked();
+        end
       
         if armyCacheInputField == nil then
-        Mod.Settings.NumOfACaches = 3;
-        Mod.Settings.Armies = 5;
-        Mod.Settings.FixedArmies = true;
-        Mod.Settings.aLuck = 5;        
+                Mod.Settings.NumOfACaches = 3;
+                Mod.Settings.Armies = 5;
+                Mod.Settings.FixedArmies = true;
+                Mod.Settings.aLuck = 5;        
         else
-        Mod.Settings.NumOfACaches = armyCacheInputField.GetValue();
-        Mod.Settings.Armies = armyAmountInputField.GetValue();
-        Mod.Settings.FixedArmies = fixedArmyInputField.GetIsChecked();
-        Mod.Settings.aLuck = randArmyInputField.GetValue();
+                Mod.Settings.NumOfACaches = armyCacheInputField.GetValue();
+                Mod.Settings.Armies = armyAmountInputField.GetValue();
+                Mod.Settings.FixedArmies = fixedArmyInputField.GetIsChecked();
+                Mod.Settings.aLuck = randArmyInputField.GetValue();
         end 
         
         if cardCacheInputField == nil then
-        
-        Mod.Settings.NumOfRCaches = 2;
-        Mod.Settings.cPieces = 3;
-        Mod.Settings.FixedPieces = true;
-        Mod.Settings.rLuck = 3;
+                Mod.Settings.NumOfRCaches = 2;
+                Mod.Settings.cPieces = 3;
+                Mod.Settings.FixedPieces = true;
+                Mod.Settings.rLuck = 3;
         else
-        Mod.Settings.NumOfRCaches = cardCacheInputField.GetValue();
-        Mod.Settings.cPieces = PiecesInputField.GetValue();
-        Mod.Settings.FixedPieces = fixedPiecesInputField.GetIsChecked();
-        Mod.Settings.rLuck = randPiecesInputField.GetValue();
+                Mod.Settings.NumOfRCaches = cardCacheInputField.GetValue();
+                Mod.Settings.cPieces = PiecesInputField.GetValue();
+                Mod.Settings.FixedPieces = fixedPiecesInputField.GetIsChecked();
+                Mod.Settings.rLuck = randPiecesInputField.GetValue();
         end
         
-   if attackNeutralInputField == nil then
-     Mod.Settings.AttackNeutral = true;
-  else
-    Mod.Settings.AttackNeutral = attackNeutralInputField.GetIsChecked();
-  end    
-
+        if attackNeutralInputField == nil then
+                Mod.Settings.AttackNeutral = true;
+        else
+                Mod.Settings.AttackNeutral = attackNeutralInputField.GetIsChecked();
+        end    
 end
