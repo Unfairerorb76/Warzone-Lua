f44828a2a35dc88eb15bd80396403ead52f02204
Jack Millar <99406899+Unfairerorb76@users.noreply.github.com>
2024-02-11 21:09:15 +0000

    Update Client_GameRefresh.lua

--- CivLight/Client_GameRefresh.lua
+++ CivLight/Client_GameRefresh.lua
@@ -4,7 +4,7 @@ function Client_GameRefresh(game)
     
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
 		UI.Alert("You must update your app to the latest version to use the Tanks mod");
-        Alerted = true;
+        	Alerted = true;
 	end
 
 end
